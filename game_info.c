3a77284bdd3440eefcf84c328c350dcd23c974d9
unknown <lloyd.aidan@gmail.com>
2013-07-16 20:37:24 -0400

    add game_info.c

---
+++ game_info.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include "chess.h"
+#define MAKE_GLOBALS_HERE
+#include "chess.glb"
+#include "chess.fun"
+#include "chess.mac"
+
+static char usage[] = "usage: game_info (-debug) filename\n";
+
+char couldnt_get_status[] = "couldn't get status of %s\n";
+char couldnt_open[] = "couldn't open %s\n";
+
+int main(int argc,char **argv)
+{
+  int n;
+  int curr_arg;
+  bool bDebug;
+  int initial_move;
+  int retval;
+  struct game curr_game;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  retval = read_game(argv[argc-1],&curr_game,err_msg);
+
+  if (retval) {
+    printf("read_game of %s failed: %d\n",argv[argc-1],retval);
+    printf("curr_move = %d\n",curr_game.curr_move);
+    return 3;
+  }
+
+  printf("%s\n",curr_game.chessfilename);
+  printf("title = %s\n",curr_game.title);
+  printf("orientation = %d\n",curr_game.orientation);
+  printf("num_moves = %d\n",curr_game.num_moves);
+
+  return 0;
+}
