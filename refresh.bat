0cce51f3c7c650dbd6c9f440bf21ec93a9fcfb95
Aidan Lloyd <lloyd.aidan@gmail.com>
2015-02-15 12:06:18 -0500

    catch up with local changes

--- refresh.bat
+++ refresh.bat
@@ -6,6 +6,17 @@ call tomysql < pop_opponent_elo_delta.sql
 call tomysql < pop_my_elo_before.sql
 call tomysql < pop_my_elo_delta.sql
 mysql_grab result
+mysql_grab result_detail
+mysql_grab wins_by_type
+mysql_grab losses_by_type
+mysql_grab white_result
+mysql_grab black_result
 mysql_grab my_elo_after
 mysql_grab my_elo_delta_by_date
 mysql_grab score_by_date
+mysql_grab first_two_moves
+mysql_grab first_two_white_moves
+mysql_grab first_two_black_moves
+mysql_grab first_two_white_moves_results
+mysql_grab first_two_black_moves_results
+mysql_grab avg_opp
