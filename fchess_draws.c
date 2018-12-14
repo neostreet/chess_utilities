b673fb171f00d91a570d62c99c7d9b5b81a42f27
Aidan Lloyd <lloyd.aidan@gmail.com>
2018-12-13 22:29:59 -0500

    add fchess_draws.c

---
+++ fchess_draws.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_FILENAME_LEN 256
+static char filename[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] =
+"usage: fchess_draws (-terse) (-is_draw) filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static char result[] = "Result \"";
+#define RESULT_LEN (sizeof (result) - 1)
+static char draw[] = "1/2-1/2";
+#define DRAW_LEN (sizeof (draw) - 1)
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index);
+
+int main(int argc,char **argv)
+{
+  int n;
+  int curr_arg;
+  bool bTerse;
+  bool bIsDraw;
+  FILE *fptr0;
+  int filename_len;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int ix;
+
+  if ((argc < 2) || (argc > 4)) {
+    printf(usage);
+    return 1;
+  }
+
+  bTerse = false;
+  bIsDraw = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-terse"))
+      bTerse = true;
+    else if (!strcmp(argv[curr_arg],"-is_draw"))
+      bIsDraw = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    if ((fptr = fopen(filename,"r")) == NULL) {
+      printf(couldnt_open,filename);
+      continue;
+    }
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      if (Contains(true,
+        line,line_len,
+        result,RESULT_LEN,
+        &ix)) {
+
+        if (Contains(true,
+          line,line_len,
+          draw,DRAW_LEN,
+          &ix)) {
+
+          if (bIsDraw)
+            printf("1 %s\n",filename);
+          else
+            printf("%s\n",filename);
+        }
+        else if (bIsDraw)
+          printf("0 %s\n",filename);
+      }
+    }
+
+    fclose(fptr);
+  }
+
+  fclose(fptr0);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
+
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index)
+{
+  int m;
+  int n;
+  int tries;
+  char chara;
+
+  tries = line_len - string_len + 1;
+
+  if (tries <= 0)
+    return false;
+
+  for (m = 0; m < tries; m++) {
+    for (n = 0; n < string_len; n++) {
+      chara = line[m + n];
+
+      if (!bCaseSens) {
+        if ((chara >= 'A') && (chara <= 'Z'))
+          chara += 'a' - 'A';
+      }
+
+      if (chara != string[n])
+        break;
+    }
+
+    if (n == string_len) {
+      *index = m;
+      return true;
+    }
+  }
+
+  return false;
+}
