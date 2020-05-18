62bc7d1336a7e6a4d7e7c2da431ddd7aa61ab56a
Aidan Lloyd <lloyd.aidan@gmail.com>
2020-05-17 21:07:52 -0400

    add -verbose, -after, and -before_and_after flags

--- fchess_elo.c
+++ fchess_elo.c
@@ -7,7 +7,8 @@ static char filename[MAX_FILENAME_LEN];
 #define MAX_LINE_LEN 1024
 static char line[MAX_LINE_LEN];
 
-static char usage[] = "usage: fchess_elo (-terse) player_name filename\n";
+static char usage[] =
+"usage: fchess_elo (-terse) (-verbose) (-after) (-before_and_after) player_name filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static char white[] = "White";
@@ -18,6 +19,10 @@ static char white_elo[] = "WhiteElo \"";
 #define WHITE_ELO_LEN (sizeof (white_elo) - 1)
 static char black_elo[] = "BlackElo \"";
 #define BLACK_ELO_LEN (sizeof (black_elo) - 1)
+static char white_rating_diff[] = "WhiteRatingDiff \"";
+#define WHITE_RATING_DIFF_LEN (sizeof (white_rating_diff) - 1)
+static char black_rating_diff[] = "BlackRatingDiff \"";
+#define BLACK_RATING_DIFF_LEN (sizeof (black_rating_diff) - 1)
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 static int Contains(bool bCaseSens,char *line,int line_len,
@@ -28,6 +33,9 @@ int main(int argc,char **argv)
   int n;
   int curr_arg;
   bool bTerse;
+  bool bVerbose;
+  bool bAfter;
+  bool bBeforeAndAfter;
   int player_name_ix;
   int player_name_len;
   FILE *fptr0;
@@ -38,17 +46,27 @@ int main(int argc,char **argv)
   bool bPlayerIsWhite;
   int ix;
   int elo;
+  int rating_diff;
 
-  if ((argc < 3) || (argc > 4)) {
+  if ((argc < 3) || (argc > 7)) {
     printf(usage);
     return 1;
   }
 
   bTerse = false;
+  bVerbose = false;
+  bAfter = false;
+  bBeforeAndAfter = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-terse"))
       bTerse = true;
+    else if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else if (!strcmp(argv[curr_arg],"-after"))
+      bAfter = true;
+    else if (!strcmp(argv[curr_arg],"-before_and_after"))
+      bBeforeAndAfter = true;
     else
       break;
   }
@@ -58,12 +76,22 @@ int main(int argc,char **argv)
     return 2;
   }
 
+  if (bTerse && bVerbose) {
+    printf("can't specify both -terse and -verbose\n");
+    return 3;
+  }
+
+  if (bAfter && bBeforeAndAfter) {
+    printf("can't specify both -before and -before_and_after\n");
+    return 4;
+  }
+
   player_name_ix = curr_arg;
   player_name_len = strlen(argv[player_name_ix]);
 
   if ((fptr0 = fopen(argv[curr_arg + 1],"r")) == NULL) {
     printf(couldnt_open,argv[curr_arg + 1]);
-    return 3;
+    return 5;
   }
 
   for ( ; ; ) {
@@ -91,12 +119,14 @@ int main(int argc,char **argv)
         if (bPlayerIsWhite) {
           sscanf(&line[ix + WHITE_ELO_LEN],"%d",&elo);
 
-          if (!bTerse)
-            printf("%4d %s\n",elo,filename);
-          else
-            printf("%d\n",elo);
+          if (!bAfter && !bBeforeAndAfter) {
+            if (bTerse)
+              printf("%d\n",elo);
+            else
+              printf("%4d %s\n",elo,filename);
 
-          break;
+            break;
+          }
         }
       }
       else if (Contains(true,
@@ -107,10 +137,72 @@ int main(int argc,char **argv)
         if (!bPlayerIsWhite) {
           sscanf(&line[ix + BLACK_ELO_LEN],"%d",&elo);
 
-          if (!bTerse)
-            printf("%4d %s\n",elo,filename);
-          else
-            printf("%d\n",elo);
+          if (!bAfter && !bBeforeAndAfter) {
+            if (bTerse)
+              printf("%d\n",elo);
+            else
+              printf("%4d %s\n",elo,filename);
+
+            break;
+          }
+        }
+      }
+      else if ((bAfter || bBeforeAndAfter) && Contains(true,
+        line,line_len,
+        white_rating_diff,WHITE_RATING_DIFF_LEN,
+        &ix)) {
+
+        if (bPlayerIsWhite) {
+          sscanf(&line[ix + WHITE_RATING_DIFF_LEN],"%d",&rating_diff);
+
+          if (bTerse) {
+            if (bAfter)
+              printf("%d\n",elo+rating_diff);
+            else
+              printf("%d %d\n",elo,elo+rating_diff);
+          }
+          else if (bVerbose) {
+            if (bAfter)
+              printf("%4d (%4d) %s\n",elo+rating_diff,rating_diff,filename);
+            else
+              printf("%4d %4d (%4d) %s\n",elo,elo+rating_diff,rating_diff,filename);
+          }
+          else {
+            if (bAfter)
+              printf("%4d %s\n",elo+rating_diff,filename);
+            else
+              printf("%4d %4d %s\n",elo,elo+rating_diff,filename);
+          }
+
+          break;
+        }
+      }
+      else if ((bAfter || bBeforeAndAfter) && Contains(true,
+        line,line_len,
+        black_rating_diff,BLACK_RATING_DIFF_LEN,
+        &ix)) {
+
+        if (!bPlayerIsWhite) {
+          sscanf(&line[ix + BLACK_RATING_DIFF_LEN],"%d",&rating_diff);
+
+          if (bTerse) {
+            if (bAfter)
+              printf("%d\n",elo+rating_diff);
+            else
+              printf("%d %d\n",elo,elo+rating_diff);
+          }
+          else if (bVerbose) {
+            if (bAfter)
+              printf("%4d (%4d) %s\n",elo+rating_diff,rating_diff,filename);
+            else
+              printf("%4d %4d (%4d) %s\n",elo,elo+rating_diff,rating_diff,filename);
+          }
+          else {
+            if (bAfter)
+              printf("%4d %s\n",elo+rating_diff,filename);
+            else
+              printf("%4d %4d %s\n",elo,elo+rating_diff,filename);
+          }
 
           break;
         }
@@ -137,7 +229,7 @@ int main(int argc,char **argv)
         else {
           printf("%s: couldn't determine whether %s is white or black\n",
             filename,argv[player_name_ix]);
-          return 4;
+          return 6;
         }
       }
     }
