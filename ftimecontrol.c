20b7359a6df924ec1a623a85b350e0e27c8143ec
Aidan Lloyd <lloyd.aidan@gmail.com>
2020-07-04 17:33:42 -0400

    add ftimecontrol.c and frapid.c

---
+++ ftimecontrol.c
@@ -0,0 +1,195 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_FILENAME_LEN 256
+static char filename[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: ftimecontrol (-debug) filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static char timecontrol[] = "TimeControl \"";
+#define TIMECONTROL_LEN (sizeof (timecontrol) - 1)
+
+struct timecontrol {
+  int seconds;
+  int increment;
+  int count;
+};
+
+#define MAX_TIMECONTROLS 100
+static struct timecontrol timecontrols[MAX_TIMECONTROLS];
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index);
+
+int main(int argc,char **argv)
+{
+  int n;
+  int curr_arg;
+  bool bDebug;
+  FILE *fptr0;
+  int filename_len;
+  FILE *fptr;
+  int line_len;
+  int ix;
+  int seconds;
+  int increment;
+  int num_timecontrols;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  num_timecontrols = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    if ((fptr = fopen(filename,"r")) == NULL) {
+      printf(couldnt_open,filename);
+      continue;
+    }
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      if (Contains(true,
+        line,line_len,
+        timecontrol,TIMECONTROL_LEN,
+        &ix)) {
+
+        for (n = ix + TIMECONTROL_LEN; n < line_len; n++) {
+          if (line[n] == '+')
+            break;
+        }
+
+        if (n < line_len) {
+          line[n++] = 0;
+          sscanf(&line[ix + TIMECONTROL_LEN],"%d",&seconds);
+          sscanf(&line[n],"%d",&increment);
+
+          for (n = 0; n < num_timecontrols; n++) {
+            if ((timecontrols[n].seconds == seconds) && (timecontrols[n].increment == increment)) {
+              break;
+            }
+          }
+
+          if (n < num_timecontrols)
+            timecontrols[n].count++;
+          else {
+            if (num_timecontrols == MAX_TIMECONTROLS) {
+              printf("MAX_TIMECONTROLS of %d exceeded\n",MAX_TIMECONTROLS);
+              return 4;
+            }
+
+            timecontrols[n].seconds = seconds;
+            timecontrols[n].increment = increment;
+            timecontrols[n].count = 1;
+            num_timecontrols++;
+          }
+        }
+
+        break;
+      }
+    }
+
+    fclose(fptr);
+  }
+
+  fclose(fptr0);
+
+  for (n = 0; n < num_timecontrols; n++) {
+    printf("%5d %d+%d\n",timecontrols[n].count,timecontrols[n].seconds,timecontrols[n].increment);
+  }
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
+
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index)
+{
+  int m;
+  int n;
+  int tries;
+  char chara;
+
+  tries = line_len - string_len + 1;
+
+  if (tries <= 0)
+    return false;
+
+  for (m = 0; m < tries; m++) {
+    for (n = 0; n < string_len; n++) {
+      chara = line[m + n];
+
+      if (!bCaseSens) {
+        if ((chara >= 'A') && (chara <= 'Z'))
+          chara += 'a' - 'A';
+      }
+
+      if (chara != string[n])
+        break;
+    }
+
+    if (n == string_len) {
+      *index = m;
+      return true;
+    }
+  }
+
+  return false;
+}
