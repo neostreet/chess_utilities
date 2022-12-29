a67eddb988270ef4c84d4abe003a25d219c4d32e
neostreet <lloyd.aidan@gmail.com>
2022-12-28 20:05:38 -0500

    add chess variant argument

--- split_etc.bat
+++ split_etc.bat
@@ -1,31 +1,31 @@
 @echo off
-split_pgn %1
-call reverse_it games.lst
-call frapid_it games.lst.rev
-ftxt2ch games.lst.rev.frapid
+split_pgn %1 %2
+call reverse_it %2_games.lst
 
-call fchess_score_plus_minus_it games.lst.rev.frapid
-call fchess_rating_diff_it games.lst.rev.frapid
-call addf_int_it games.lst.rev.frapid.fchess_rating_diff
+ftxt2ch %2_games.lst.rev
 
-call fchess_wins_it games.lst.rev.frapid
-rem call fchess_wins_terse_it games.lst.rev.frapid
-call fchess_rating_diff_it games.lst.rev.frapid.fchess_wins
-call addf_int_it games.lst.rev.frapid.fchess_wins.fchess_rating_diff
+call fchess_score_plus_minus_it %2_games.lst.rev
+call fchess_rating_diff_it %2_games.lst.rev
+call addf_int_it %2_games.lst.rev.fchess_rating_diff
 
-call fchess_draws_it games.lst.rev.frapid
-rem call fchess_draws_terse_it games.lst.rev.frapid
-call fchess_rating_diff_it games.lst.rev.frapid.fchess_draws
-call addf_int_it games.lst.rev.frapid.fchess_draws.fchess_rating_diff
+call fchess_wins_it %2_games.lst.rev
+rem call fchess_wins_terse_it %2_games.lst.rev
+call fchess_rating_diff_it %2_games.lst.rev.fchess_wins
+call addf_int_it %2_games.lst.rev.fchess_wins.fchess_rating_diff
 
-call fchess_losses_it games.lst.rev.frapid
-rem call fchess_losses_terse_it games.lst.rev.frapid
-call fchess_rating_diff_it games.lst.rev.frapid.fchess_losses
-call addf_int_it games.lst.rev.frapid.fchess_losses.fchess_rating_diff
+call fchess_draws_it %2_games.lst.rev
+rem call fchess_draws_terse_it %2_games.lst.rev
+call fchess_rating_diff_it %2_games.lst.rev.fchess_draws
+call addf_int_it %2_games.lst.rev.fchess_draws.fchess_rating_diff
 
-rem type games.lst.rev.frapid.fchess_score
-call lichess_lines9
-rem type games.lst.rev.frapid.fchess_wins.fchess_rating_diff.addf_int
-rem type games.lst.rev.frapid.fchess_draws.fchess_rating_diff.addf_int
-rem type games.lst.rev.frapid.fchess_losses.fchess_rating_diff.addf_int
-rem type games.lst.rev.frapid.fchess_rating_diff.addf_int
+call fchess_losses_it %2_games.lst.rev
+rem call fchess_losses_terse_it %2_games.lst.rev
+call fchess_rating_diff_it %2_games.lst.rev.fchess_losses
+call addf_int_it %2_games.lst.rev.fchess_losses.fchess_rating_diff
+
+rem type %2_games.lst.rev.fchess_score
+call lichess_lines9 %2
+rem type %2_games.lst.rev.fchess_wins.fchess_rating_diff.addf_int
+rem type %2_games.lst.rev.fchess_draws.fchess_rating_diff.addf_int
+rem type %2_games.lst.rev.fchess_losses.fchess_rating_diff.addf_int
+rem type %2_games.lst.rev.fchess_rating_diff.addf_int
