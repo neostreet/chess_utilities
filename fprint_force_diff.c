e4211ce20e0072d3689b940d0e4dfe7bd27a4d92
neostreet <lloyd.aidan@gmail.com>
2023-06-30 14:06:15 -0400

    write out a separate output file for each input file

--- fprint_force_diff.c
+++ fprint_force_diff.c
@@ -13,6 +13,7 @@ using namespace std;
 
 #define MAX_FILENAME_LEN 256
 static char filename[MAX_FILENAME_LEN];
+static char out_filename[MAX_FILENAME_LEN];
 
 static char usage[] = "usage: fprint_force_diff (-debug) filename\n";
 
@@ -27,6 +28,7 @@ int main(int argc,char **argv)
   int curr_arg;
   bool bDebug;
   FILE *fptr;
+  FILE *out_fptr;
   int filename_len;
   int retval;
   struct game curr_game;
@@ -60,24 +62,30 @@ int main(int argc,char **argv)
   retval = read_game(filename,&curr_game,err_msg);
 
   if (retval) {
-    printf("read_game of %s failed: %d\n",argv[argc-1],retval);
+    printf("read_game of %s failed: %d\n",filename,retval);
     printf("curr_move = %d\n",curr_game.curr_move);
     continue;
   }
 
-  printf("%s\n\n",filename);
+  sprintf(out_filename,"%s.force_diff",filename);
+
+  if ((out_fptr = fopen(out_filename,"w")) == NULL) {
+    printf(couldnt_open,out_filename);
+    return 3;
+  }
 
   set_initial_board(&curr_game);
 
-  printf("%d %d\n",curr_game.force_count[WHITE],curr_game.force_count[WHITE] - curr_game.force_count[BLACK]);
+  fprintf(out_fptr,"%d %d 0\n",curr_game.force_count[WHITE],curr_game.force_count[WHITE] - curr_game.force_count[BLACK]);
 
   for (curr_game.curr_move = 0; curr_game.curr_move < curr_game.num_moves; curr_game.curr_move++) {
     update_board(&curr_game,false);
     refresh_force_count(&curr_game);
-    printf("%d %d\n",curr_game.force_count[WHITE],curr_game.force_count[WHITE] - curr_game.force_count[BLACK]);
+    fprintf(out_fptr,"%d %d %d\n",curr_game.force_count[WHITE],curr_game.force_count[WHITE] - curr_game.force_count[BLACK],
+      curr_game.curr_move + 1);
   }
 
-  putchar(0x0a);
+  fclose(out_fptr);
 
   }
 
