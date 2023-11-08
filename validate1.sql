9f5304d73b79fdc58e892bdf75b994aafe89f6c7
neostreet <lloyd.aidan@gmail.com>
2023-11-08 11:31:09 -0500

    Update validate1.sql

--- validate1.sql
+++ validate1.sql
@@ -1,7 +1,8 @@
 use chess
 
-select count(*) from blitz_games curr
-inner join blitz_games prev on curr.id = prev.id + 1
-where curr.my_elo_before != prev.my_elo_after;
+select prev.id,prev.game_date prev_game_date,(prev.my_elo_before + prev.my_elo_delta) prev_after,
+curr.id,curr.game_date curr_game_date,(curr.my_elo_before) curr_before
+from rapid_games curr inner join rapid_games prev on curr.id = prev.id + 1
+where curr.my_elo_before != prev.my_elo_before + prev.my_elo_delta;
 
 quit
