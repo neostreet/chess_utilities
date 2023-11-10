1dafe64322d8d9cff06cc57b8a3ff6684056a111
neostreet <lloyd.aidan@gmail.com>
2023-11-10 16:18:08 -0500

    make the blitz table consistent with the rapid table

--- blitz_games_table.sql
+++ blitz_games_table.sql
@@ -4,25 +4,23 @@ drop table if exists blitz_games;
 
 create table blitz_games (
   id mediumint auto_increment primary key,
-  blitz_game_date date not null,
+  game_filename varchar(30) not null,
+  game_date date not null,
   time_control varchar(10) not null,
   opponent_name varchar(25) not null,
   color varchar(1) not null,
   num_moves smallint not null,
+  my_checks smallint not null,
+  opponent_checks smallint not null,
   result varchar(1) not null,
-  result_decimal decimal(6,1) null,
+  mate varchar(1) not null,
   result_detail varchar(25) not null,
-  opponent_elo_before int null,
-  opponent_elo_after int not null,
-  opponent_elo_delta smallint null,
-  my_elo_before int null,
-  my_elo_after int not null,
-  my_elo_delta smallint null,
-  first_move varchar(10) null,
-  first_two_moves varchar(25) null,
-  first_three_moves varchar(35) null,
-  first_four_moves varchar(45) null,
-  eco varchar(3) null
+  my_elo_before int not null,
+  my_elo_delta smallint not null,
+  opponent_elo_before int not null,
+  opponent_elo_delta smallint not null,
+  eco varchar(4) not null,
+  opening varchar(150) not null
 );
 
 quit
