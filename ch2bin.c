3f288fd2166ec59d8cfd84144857ea8fa3fac122
neostreet <lloyd.aidan@gmail.com>
2024-02-09 17:24:38 -0500

    add ch2bin.c

---
+++ ch2bin.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "chess.h"
+#define MAKE_GLOBALS_HERE
+#include "chess.glb"
+#include "chess.fun"
+#include "chess.mac"
+
+static char usage[] =
+"usage: ch2bin (-debug) infile outfile\n";
+
+char couldnt_get_status[] = "couldn't get status of %s\n";
+char couldnt_open[] = "couldn't open %s\n";
+
+void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bDebug;
+  int retval;
+  struct game curr_game;
+
+  if ((argc < 3) || (argc > 4)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 2) {
+    printf(usage);
+    return 2;
+  }
+
+  retval = read_game(argv[curr_arg],&curr_game,err_msg);
+
+  if (retval) {
+    printf("read_game of %s failed: %d\n",argv[curr_arg],retval);
+    printf("curr_move = %d\n",curr_game.curr_move);
+    return 3;
+  }
+
+  retval = write_binary_game(argv[curr_arg+1],&curr_game);
+
+  if (retval) {
+    printf("write_binary_game of %s failed: %d\n",argv[curr_arg+1],retval);
+    return 4;
+  }
+
+  return 0;
+}
