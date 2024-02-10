068c45866392ff5a70720375f833044bbaaf0219
neostreet <lloyd.aidan@gmail.com>
2024-02-10 16:04:57 -0500

    add orientation argument

--- print_bd0.c
+++ print_bd0.c
@@ -7,7 +7,7 @@
 #include "chess.fun"
 #include "chess.mac"
 
-static char usage[] = "usage: print_bd0 filename\n";
+static char usage[] = "usage: print_bd0 orientation filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
@@ -17,20 +17,23 @@ static unsigned char board1[CHARS_IN_BOARD];
 int main(int argc,char **argv)
 {
   int retval;
+  int orientation;
 
-  if (argc != 2 ) {
+  if (argc != 3) {
     printf(usage);
     return 1;
   }
 
-  retval = populate_board_from_bin_board_file(board1,argv[1]);
+  sscanf(argv[1],"%d",&orientation);
+
+  retval = populate_board_from_bin_board_file(board1,argv[2]);
 
   if (retval) {
-    printf("populate_board_from_bin_board_file() failed on %s: %d\n",argv[1],retval);
+    printf("populate_board_from_bin_board_file() failed on %s: %d\n",argv[2],retval);
     return 2;
   }
 
-  print_bd0(board1);
+  print_bd0(board1,orientation);
 
   return 0;
 }
