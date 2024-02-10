18ba2e512e85100d368017b7c2fa540fe6fd2d5a
neostreet <lloyd.aidan@gmail.com>
2024-02-10 18:42:04 -0500

    remove black_to_play and moved_piece

--- chess.h
+++ chess.h
@@ -89,16 +89,15 @@ struct move {
 #define ANNOTATION_X (8 * XLEN + 2 + FONT_WIDTH)
 #define ANNOTATION_Y 5
 
-#define MAX_FILE_NAME_LEN 256
+#define MAX_FILE_NAME_LEN 128
+#define MAX_TITLE_LEN 128
 
 struct game {
   char chessfilename[MAX_FILE_NAME_LEN];
-  char title[80];
-  int black_to_play;
+  char title[MAX_TITLE_LEN];
   int orientation;
   int num_moves;
   int curr_move;
-  int moved_piece;
   unsigned char board[CHARS_IN_BOARD];  /* 8 columns * 8 rows / 2 (nibbles per char) */
   short force_count[NUM_PLAYERS];
   struct move moves[MAX_MOVES];
