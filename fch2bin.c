35f4215f4f54bd34a2142a79f5cc0b6e609b9ad3
neostreet <lloyd.aidan@gmail.com>
2024-02-09 21:35:57 -0500

    add fch2bin.c

---
+++ fch2bin.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_FILENAME_LEN 1024
+
+static char filename[MAX_FILENAME_LEN];
+static char bin_filename[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 8192
+static char line[MAX_LINE_LEN];
+
+#include "chess.h"
+#define MAKE_GLOBALS_HERE
+#include "chess.glb"
+#include "chess.fun"
+#include "chess.mac"
+
+static char usage[] =
+"usage: fch2bin (-debug) filename\n";
+
+char couldnt_get_status[] = "couldn't get status of %s\n";
+char couldnt_open[] = "couldn't open %s\n";
+
+static int build_bin_filename(
+  char *ch_filename,
+  int ch_filename_len,
+  char *bin_filename,
+  int max_filename_len);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bDebug;
+  FILE *fptr0;
+  int file_len;
+  int ch_filename_len;
+  int retval;
+  struct game curr_game;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&file_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    ch_filename_len = strlen(filename);
+
+    retval = build_bin_filename(filename,ch_filename_len,bin_filename,MAX_FILENAME_LEN);
+
+    if (retval) {
+      printf("build_bin_filename failed on %s: %d\n",filename,retval);
+      continue;
+    }
+
+    retval = read_game(filename,&curr_game,err_msg);
+
+    if (retval) {
+      printf("read_game of %s failed: %d\n",filename,retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+      continue;
+    }
+
+    retval = write_binary_game(bin_filename,&curr_game);
+
+    if (retval) {
+      printf("write_binary_game of %s failed: %d\n",bin_filename,retval);
+      continue;
+    }
+  }
+
+  return 0;
+}
+
+static int build_bin_filename(
+  char *ch_filename,
+  int ch_filename_len,
+  char *bin_filename,
+  int max_filename_len)
+{
+  int n;
+
+  for (n = 0; n < ch_filename_len; n++) {
+    if (ch_filename[n] == '.')
+      break;
+  }
+
+  if (n == ch_filename_len)
+    return 1;
+
+  if (n + 4 > max_filename_len - 1)
+    return 2;
+
+  strncpy(bin_filename,ch_filename,n);
+  strcpy(&bin_filename[n],".bin");
+
+  return 0;
+}
