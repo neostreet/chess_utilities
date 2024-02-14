5bd69a3db17bedabcb1e26b1a4250e4243f8d8bb
neostreet <lloyd.aidan@gmail.com>
2024-02-13 19:51:51 -0500

    add -binary_format flag

--- prgame.c
+++ prgame.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "chess.h"
 #define MAKE_GLOBALS_HERE
@@ -6,25 +7,54 @@
 #include "chess.fun"
 #include "chess.mac"
 
+static char usage[] = "usage: prgame (-binary_format) filename\n";
+
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
 
 int main(int argc,char **argv)
 {
+  int curr_arg;
+  bool bBinaryFormat;
   int retval;
   struct game curr_game;
 
-  if (argc != 2) {
-    printf("usage: prgame filename\n");
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
     return 1;
   }
 
-  retval = read_game(argv[1],&curr_game,err_msg);
+  bBinaryFormat = false;
 
-  if (retval) {
-    printf("read_game of %s failed: %d\n",argv[1],retval);
-    printf("curr_move = %d\n",curr_game.curr_move);
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-binary_format"))
+      bBinaryFormat = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
     return 2;
+  }
+
+  if (!bBinaryFormat) {
+    retval = read_game(argv[curr_arg],&curr_game,err_msg);
+
+    if (retval) {
+      printf("read_game of %s failed: %d\n",argv[curr_arg],retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+      return 3;
+    }
+  }
+  else {
+    retval = read_binary_game(argv[curr_arg],&curr_game);
+
+    if (retval) {
+      printf("read_binary_game of %s failed: %d\n",argv[curr_arg],retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+      return 4;
+    }
   }
 
   print_game(&curr_game);
