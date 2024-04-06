531111878863eb45ade339fa18fef0e92b9a216d
neostreet <lloyd.aidan@gmail.com>
2024-04-05 22:19:03 -0400

    add -verbose flag

--- count_checks.c
+++ count_checks.c
@@ -11,7 +11,7 @@
 static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
-"usage: count_checks (-debug) filename\n";
+"usage: count_checks (-verbose) filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
@@ -20,24 +20,25 @@ int main(int argc,char **argv)
 {
   int n;
   int curr_arg;
-  bool bDebug;
+  bool bVerbose;
   int retval;
   FILE *fptr;
   int filename_len;
   struct game curr_game;
   bool bBlack;
-  int num_checks;
+  int num_white_checks;
+  int num_black_checks;
 
   if ((argc < 2) || (argc > 3)) {
     printf(usage);
     return 1;
   }
 
-  bDebug = false;
+  bVerbose = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
-    if (!strcmp(argv[curr_arg],"-debug"))
-      bDebug = true;
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
     else
       break;
   }
@@ -67,17 +68,27 @@ int main(int argc,char **argv)
 
     set_initial_board(&curr_game);
 
-    num_checks = 0;
+    num_white_checks = 0;
+    num_black_checks = 0;
 
     for (curr_game.curr_move = 0; curr_game.curr_move < curr_game.num_moves; curr_game.curr_move++) {
       bBlack = curr_game.curr_move & 0x1;
       update_board(curr_game.board,&curr_game.moves[curr_game.curr_move],bBlack);
 
-      if (curr_game.moves[curr_game.curr_move].special_move_info & SPECIAL_MOVE_CHECK)
-        num_checks++;
+      if (curr_game.moves[curr_game.curr_move].special_move_info & SPECIAL_MOVE_CHECK) {
+        if (bBlack)
+          num_black_checks++;
+        else
+          num_white_checks++;
+      }
     }
 
-    printf("%d %s\n",num_checks,filename);
+    if (bVerbose) {
+      printf("%d white %d black %d total %s\n",
+        num_white_checks,num_black_checks,num_white_checks + num_black_checks,filename);
+    }
+    else
+      printf("%d %s\n",num_white_checks + num_black_checks,filename);
   }
 
   return 0;
