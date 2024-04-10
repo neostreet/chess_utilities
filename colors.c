19a1ef9cadce8a3a766172d2ec8fbb8d9c8d6c9d
neostreet <lloyd.aidan@gmail.com>
2024-04-09 21:20:11 -0400

    add colors.c

---
+++ colors.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "chess.h"
+#define MAKE_GLOBALS_HERE
+#include "chess.glb"
+#include "chess.fun"
+#include "chess.mac"
+
+#define MAX_FILENAME_LEN 256
+static char filename[MAX_FILENAME_LEN];
+
+static char usage[] =
+"usage: colors filename\n";
+
+char couldnt_get_status[] = "couldn't get status of %s\n";
+char couldnt_open[] = "couldn't open %s\n";
+
+int main(int argc,char **argv)
+{
+  int n;
+  int retval;
+  FILE *fptr;
+  int filename_len;
+  struct game curr_game;
+  int whites;
+  int blacks;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  whites = 0;
+  blacks = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr))
+      break;
+
+    retval = read_game(filename,&curr_game,err_msg);
+
+    if (retval) {
+      printf("read_game of %s failed: %d\n",filename,retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+      continue;
+    }
+
+    if (!curr_game.orientation)
+      whites++;
+    else
+      blacks++;
+  }
+
+  fclose(fptr);
+
+  printf("%6d whites\n",whites);
+  printf("%6d blacks\n",blacks);
+  printf("=============\n");
+  printf("%6d games\n",whites + blacks);
+
+  return 0;
+}
