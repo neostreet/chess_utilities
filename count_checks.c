1a055a3e715a327fbe41321aa20f6296fc50c652
neostreet <lloyd.aidan@gmail.com>
2024-05-10 17:34:59 -0400

    add -binary_format flag

--- count_checks.c
+++ count_checks.c
@@ -12,7 +12,7 @@ static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
 "usage: count_checks (-debug) (-verbose) (-consecutive) (-game_ending) (-game_ending_countcount) (-by_player)\n"
-" (-mate) (-mate_piecepiece) (-none) filename\n";
+" (-mate) (-mate_piecepiece) (-none) (-binary_format) filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
@@ -28,6 +28,7 @@ int main(int argc,char **argv)
   bool bByPlayer;
   bool bMate;
   bool bNone;
+  bool bBinaryFormat;
   int game_ending_count;
   int retval;
   FILE *fptr;
@@ -48,7 +49,7 @@ int main(int argc,char **argv)
   int last_piece;
   char last_piece_char;
 
-  if ((argc < 2) || (argc > 11)) {
+  if ((argc < 2) || (argc > 12)) {
     printf(usage);
     return 1;
   }
@@ -62,6 +63,7 @@ int main(int argc,char **argv)
   bMate = false;
   mate_piece = ' ';
   bNone = false;
+  bBinaryFormat = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-debug"))
@@ -82,6 +84,8 @@ int main(int argc,char **argv)
       mate_piece = argv[curr_arg][11];
     else if (!strcmp(argv[curr_arg],"-none"))
       bNone = true;
+    else if (!strcmp(argv[curr_arg],"-binary_format"))
+      bBinaryFormat = true;
     else
       break;
   }
@@ -116,10 +120,25 @@ int main(int argc,char **argv)
 
     retval = read_game(filename,&curr_game,err_msg);
 
-    if (retval) {
-      printf("read_game of %s failed: %d\n",filename,retval);
-      printf("curr_move = %d\n",curr_game.curr_move);
-      continue;
+    if (!bBinaryFormat) {
+      retval = read_game(filename,&curr_game,err_msg);
+
+      if (retval) {
+        printf("read_game of %s failed: %d\n",filename,retval);
+        printf("curr_move = %d\n",curr_game.curr_move);
+
+        continue;
+      }
+    }
+    else {
+      retval = read_binary_game(filename,&curr_game);
+
+      if (retval) {
+        printf("read_binary_game of %s failed: %d\n",filename,retval);
+        printf("curr_move = %d\n",curr_game.curr_move);
+
+        continue;
+      }
     }
 
     if (bGameEnding) {
