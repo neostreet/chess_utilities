3af62a07419ecfb699c9b5d1149c4f6c789f0f5e
neostreet <lloyd.aidan@gmail.com>
2024-05-10 17:57:11 -0400

    add -binary_format flag

--- count_mate_pieces.c
+++ count_mate_pieces.c
@@ -11,7 +11,7 @@
 static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
-"usage: count_mate_pieces (-verbose) filename\n";
+"usage: count_mate_pieces (-verbose) (-binary_format) filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
@@ -21,6 +21,7 @@ int main(int argc,char **argv)
   int n;
   int curr_arg;
   bool bVerbose;
+  bool bBinaryFormat;
   int retval;
   FILE *fptr;
   int filename_len;
@@ -30,16 +31,19 @@ int main(int argc,char **argv)
   int ix;
   int total_counts;
 
-  if ((argc < 2) || (argc > 3)) {
+  if ((argc < 2) || (argc > 4)) {
     printf(usage);
     return 1;
   }
 
   bVerbose = false;
+  bBinaryFormat = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-verbose"))
       bVerbose = true;
+    else if (!strcmp(argv[curr_arg],"-binary_format"))
+      bBinaryFormat = true;
     else
       break;
   }
@@ -63,12 +67,25 @@ int main(int argc,char **argv)
     if (feof(fptr))
       break;
 
-    retval = read_game(filename,&curr_game,err_msg);
+    if (!bBinaryFormat) {
+      retval = read_game(filename,&curr_game,err_msg);
 
-    if (retval) {
-      printf("read_game of %s failed: %d\n",filename,retval);
-      printf("curr_move = %d\n",curr_game.curr_move);
-      continue;
+      if (retval) {
+        printf("read_game of %s failed: %d\n",filename,retval);
+        printf("curr_move = %d\n",curr_game.curr_move);
+
+        continue;
+      }
+    }
+    else {
+      retval = read_binary_game(filename,&curr_game);
+
+      if (retval) {
+        printf("read_binary_game of %s failed: %d\n",filename,retval);
+        printf("curr_move = %d\n",curr_game.curr_move);
+
+        continue;
+      }
     }
 
     if (curr_game.moves[curr_game.num_moves-1].special_move_info & SPECIAL_MOVE_MATE) {
