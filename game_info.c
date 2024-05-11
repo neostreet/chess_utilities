d7f09378f01ecf153c00e2ed9917e455bd2f116f
neostreet <lloyd.aidan@gmail.com>
2024-05-10 23:16:41 -0400

    add -binary_format flag

--- game_info.c
+++ game_info.c
@@ -6,7 +6,7 @@
 #include "chess.fun"
 #include "chess.mac"
 
-static char usage[] = "usage: game_info (-debug) filename\n";
+static char usage[] = "usage: game_info (-debug) (-binary_format) filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
@@ -16,20 +16,24 @@ int main(int argc,char **argv)
   int n;
   int curr_arg;
   bool bDebug;
+  bool bBinaryFormat;
   int initial_move;
   int retval;
   struct game curr_game;
 
-  if ((argc < 2) || (argc > 3)) {
+  if ((argc < 2) || (argc > 4)) {
     printf(usage);
     return 1;
   }
 
   bDebug = false;
+  bBinaryFormat = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-debug"))
       bDebug = true;
+    else if (!strcmp(argv[curr_arg],"-binary_format"))
+      bBinaryFormat = true;
     else
       break;
   }
@@ -39,15 +43,28 @@ int main(int argc,char **argv)
     return 2;
   }
 
-  retval = read_game(argv[argc-1],&curr_game,err_msg);
+  if (!bBinaryFormat) {
+    retval = read_game(argv[curr_arg],&curr_game,err_msg);
 
-  if (retval) {
-    printf("read_game of %s failed: %d\n",argv[argc-1],retval);
-    printf("curr_move = %d\n",curr_game.curr_move);
-    return 3;
+    if (retval) {
+      printf("read_game of %s failed: %d\n",argv[curr_arg],retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+
+      return 3;
+    }
+  }
+  else {
+    retval = read_binary_game(argv[curr_arg],&curr_game);
+
+    if (retval) {
+      printf("read_binary_game of %s failed: %d\n",argv[curr_arg],retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+
+      return 4;
+    }
   }
 
-  printf("%s\n",argv[argc-1]);
+  printf("%s\n",argv[curr_arg]);
   printf("title = %s\n",curr_game.title);
   printf("orientation = %d\n",curr_game.orientation);
   printf("num_moves = %d\n",curr_game.num_moves);
