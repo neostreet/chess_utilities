729d080df59de185c3b5f407e5d65aa30b048be3
neostreet <lloyd.aidan@gmail.com>
2024-05-20 12:16:44 -0400

    added print_piece_info.c, print_piece_info2.c, and populate_board_from_piece_info.c

---
+++ print_piece_info.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "chess.h"
+#define MAKE_GLOBALS_HERE
+#include "chess.glb"
+#include "chess.fun"
+#include "chess.mac"
+
+static char usage[] =
+"usage: print_piece_info (-debug) filename\n";
+
+char couldnt_get_status[] = "couldn't get status of %s\n";
+char couldnt_open[] = "couldn't open %s\n";
+
+void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bDebug;
+  int retval;
+  struct game curr_game;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  retval = read_game(argv[curr_arg],&curr_game,err_msg);
+
+  if (retval) {
+    printf("read_game of %s failed: %d\n",argv[curr_arg],retval);
+    printf("curr_move = %d\n",curr_game.curr_move);
+    return 3;
+  }
+
+  printf("%s\n",argv[curr_arg]);
+
+  print_piece_info(&curr_game);
+
+  return 0;
+}
