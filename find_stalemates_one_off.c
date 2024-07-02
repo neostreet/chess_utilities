6182c78056eda6bdea21090b38b877abec273573
neostreet <lloyd.aidan@gmail.com>
2024-07-02 19:47:16 -0400

    add  find_stalemates_one_off.c

---
+++ find_stalemates_one_off.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "chess.h"
+#define MAKE_GLOBALS_HERE
+#include "chess.glb"
+#include "chess.fun"
+#include "chess.mac"
+
+static char usage[] =
+"usage: find_stalemates_one_off (-binary_format) filename\n";
+
+char couldnt_get_status[] = "couldn't get status of %s\n";
+char couldnt_open[] = "couldn't open %s\n";
+
+int main(int argc,char **argv)
+{
+  int n;
+  int curr_arg;
+  bool bBinaryFormat;
+  int retval;
+  FILE *fptr;
+  struct game curr_game;
+  unsigned char board[CHARS_IN_BOARD];
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bBinaryFormat = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-binary_format"))
+      bBinaryFormat = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  bzero(&curr_game,sizeof (struct game));
+
+  if (!bBinaryFormat) {
+    retval = read_game(argv[curr_arg],&curr_game,err_msg);
+
+    if (retval) {
+      printf("read_game of %s failed: %d\n",argv[curr_arg],retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+
+      return 3;
+    }
+  }
+  else {
+    retval = read_binary_game(argv[curr_arg],&curr_game);
+
+    if (retval) {
+      printf("read_binary_game of %s failed: %d\n",argv[curr_arg],retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+
+      return 4;
+    }
+  }
+
+  if (curr_game.moves[curr_game.num_moves-1].special_move_info & SPECIAL_MOVE_STALEMATE)
+    printf("%s\n",argv[curr_arg]);
+
+  return 0;
+}
