8062f2a3920df4c65d2eeb0f40abc17cafd3c64f
neostreet <lloyd.aidan@gmail.com>
2024-07-08 15:23:37 -0400

    add -mating_square argument

--- find_mates.c
+++ find_mates.c
@@ -11,7 +11,7 @@
 static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
-"usage: find_mates (-binary_format) (-mine) (-not_mine) (-white) (-black) filename\n";
+"usage: find_mates (-binary_format) (-mine) (-not_mine) (-white) (-black) (-mating_squaresquare) filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
@@ -25,13 +25,16 @@ int main(int argc,char **argv)
   bool bNotMine;
   bool bWhite;
   bool bBlack;
+  int file;
+  int rank;
+  int mating_square;
   int retval;
   FILE *fptr;
   int filename_len;
   struct game curr_game;
   unsigned char board[CHARS_IN_BOARD];
 
-  if ((argc < 2) || (argc > 7)) {
+  if ((argc < 2) || (argc > 8)) {
     printf(usage);
     return 1;
   }
@@ -41,6 +44,7 @@ int main(int argc,char **argv)
   bNotMine = false;
   bWhite = false;
   bBlack = false;
+  mating_square = -1;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-binary_format"))
@@ -53,6 +57,15 @@ int main(int argc,char **argv)
       bWhite = true;
     else if (!strcmp(argv[curr_arg],"-black"))
       bBlack = true;
+    else if ((strlen(argv[curr_arg]) == 16) &&
+      !strncmp(argv[curr_arg],"-mating_square",14) &&
+      (argv[curr_arg][14] >= 'a') && (argv[curr_arg][14] <= 'h') &&
+      (argv[curr_arg][15] >= '1') && (argv[curr_arg][15] <= '8')) {
+
+      file = argv[curr_arg][14] - 'a';
+      rank = argv[curr_arg][15] - '1';
+      mating_square = POS_OF(rank,file);
+    }
     else
       break;
   }
@@ -107,6 +120,11 @@ int main(int argc,char **argv)
     }
 
     if (curr_game.moves[curr_game.num_moves-1].special_move_info & SPECIAL_MOVE_MATE) {
+      if (mating_square != -1 ) {
+        if (curr_game.moves[curr_game.num_moves-1].to != mating_square)
+          continue;
+      }
+
       if (bWhite) {
         if (!(curr_game.num_moves % 2))
           continue;
