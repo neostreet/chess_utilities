db34d168fb4f1672bd7bc683ca3ee95d80698d7c
neostreet <lloyd.aidan@gmail.com>
2024-07-24 14:52:48 -0400

    add find_missed_mates.c

---
+++ find_missed_mates.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "chess.h"
+#define MAKE_GLOBALS_HERE
+#include "chess.glb"
+#include "chess.fun"
+#include "chess.mac"
+
+#define MAX_FILENAME_LEN 256
+static char filename[MAX_FILENAME_LEN];
+
+static char usage[] =
+"usage: find_missed_mates -binary_format filename\n";
+
+char couldnt_get_status[] = "couldn't get status of %s\n";
+char couldnt_open[] = "couldn't open %s\n";
+
+static struct move work_legal_moves[MAX_LEGAL_MOVES];
+
+int main(int argc,char **argv)
+{
+  int n;
+  int curr_arg;
+  bool bBinaryFormat;
+  int retval;
+  FILE *fptr;
+  int filename_len;
+  struct game curr_game;
+  struct game work_game;
+  int work_legal_moves_count;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bBinaryFormat = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-binary_format"))
+      bBinaryFormat = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  for ( ; ; ) {
+    GetLine(fptr,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr))
+      break;
+
+    bzero(&curr_game,sizeof (struct game));
+
+    if (!bBinaryFormat) {
+      retval = read_game(filename,&curr_game,err_msg);
+
+      if (retval) {
+        printf("read_game of %s failed: %d\n",filename,retval);
+        printf("curr_move = %d\n",curr_game.curr_move);
+
+        continue;
+      }
+    }
+    else {
+      retval = read_binary_game(filename,&curr_game);
+
+      if (retval) {
+        printf("read_binary_game of %s failed: %d\n",filename,retval);
+        printf("curr_move = %d\n",curr_game.curr_move);
+
+        continue;
+      }
+    }
+
+    set_initial_board(&curr_game);
+
+    for (curr_game.curr_move = 0; curr_game.curr_move < curr_game.num_moves; curr_game.curr_move++) {
+      legal_moves_count = 0;
+      get_legal_moves(&curr_game,legal_moves,&legal_moves_count);
+
+      for (n = 0; n < legal_moves_count; n++) {
+        copy_game(&work_game,&curr_game);
+        work_game.moves[work_game.curr_move].from = legal_moves[n].from;
+        work_game.moves[work_game.curr_move].to = legal_moves[n].to;
+        work_game.moves[work_game.curr_move].special_move_info = 0;
+        update_board(&work_game,NULL,NULL,true);
+        work_game.curr_move++;
+        work_legal_moves_count = 0;
+        get_legal_moves(&work_game,work_legal_moves,&work_legal_moves_count);
+
+        if (!work_legal_moves_count) {
+          printf("%s: a mate was missed on move %d:\n",filename,curr_game.curr_move);
+          print_bd(&work_game);
+          break;
+        }
+      }
+
+      update_board(&curr_game,NULL,NULL,false);
+      update_piece_info(&curr_game);
+    }
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
