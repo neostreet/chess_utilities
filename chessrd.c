neostreet/chess_utilities

commit 4f775d11719701dda453f987595f71f202b7e51f

skip special move processing on scratch moves

chessrd.c
@@ -517,20 +517,22 @@ void update_board(struct game *gamept,int *invalid_squares,int *num_invalid_squa
       update_board_calls,gamept->curr_move,gamept->moves[gamept->curr_move].special_move_info);
   }
 
-  if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_KINGSIDE_CASTLE)
-    bKingsideCastle = true;
-  else if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_QUEENSIDE_CASTLE)
-    bQueensideCastle = true;
-  else if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_EN_PASSANT_CAPTURE)
-    bEnPassantCapture = true;
-  else if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_PROMOTION_QUEEN)
-    from_piece = (bBlack ? QUEEN_ID * -1 : QUEEN_ID);
-  else if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_PROMOTION_ROOK)
-    from_piece = (bBlack ? ROOK_ID * -1 : ROOK_ID);
-  else if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_PROMOTION_BISHOP)
-    from_piece = (bBlack ? BISHOP_ID * -1 : BISHOP_ID);
-  else if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_PROMOTION_KNIGHT)
-    from_piece = (bBlack ? KNIGHT_ID * -1 : KNIGHT_ID);
+  if (!bScratch) {
+    if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_KINGSIDE_CASTLE)
+      bKingsideCastle = true;
+    else if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_QUEENSIDE_CASTLE)
+      bQueensideCastle = true;
+    else if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_EN_PASSANT_CAPTURE)
+      bEnPassantCapture = true;
+    else if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_PROMOTION_QUEEN)
+      from_piece = (bBlack ? QUEEN_ID * -1 : QUEEN_ID);
+    else if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_PROMOTION_ROOK)
+      from_piece = (bBlack ? ROOK_ID * -1 : ROOK_ID);
+    else if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_PROMOTION_BISHOP)
+      from_piece = (bBlack ? BISHOP_ID * -1 : BISHOP_ID);
+    else if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_PROMOTION_KNIGHT)
+      from_piece = (bBlack ? KNIGHT_ID * -1 : KNIGHT_ID);
+  }
 
   if (invalid_squares) {
     *num_invalid_squares = 0;

commit e492334f7c79375897121e1dc98daec8d77e8ec6

fix bug in update_board() which was introduced in commit 4f775d11

chessrd.c
@@ -517,7 +517,7 @@ void update_board(struct game *gamept,int *invalid_squares,int *num_invalid_squa
       update_board_calls,gamept->curr_move,gamept->moves[gamept->curr_move].special_move_info);
   }
 
-  if (!bScratch) {
+  if (!bInGetLegalMoves) {
     if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_KINGSIDE_CASTLE)
       bKingsideCastle = true;
     else if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_QUEENSIDE_CASTLE)