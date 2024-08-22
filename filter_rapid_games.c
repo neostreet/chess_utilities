e6bafef0edd3bd609ec98481328d9796cb5a3e77
neostreet <lloyd.aidan@gmail.com>
2024-08-22 11:43:54 -0400

    add filter_rapid_games.c

---
+++ filter_rapid_games.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_FILENAME_LEN 256
+static char filename[MAX_FILENAME_LEN];
+static char filename2[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: filter_rapid_games (-verbose) (-rename) filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static char *rapid_time_controls[] = {
+  "900+10",
+  "600+5",
+  "600+0",
+  "600+3"
+};
+#define NUM_RAPID_TIME_CONTROLS (sizeof rapid_time_controls / sizeof (char *))
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bVerbose;
+  bool bRename;
+  int n;
+  FILE *fptr0;
+  int filename_len;
+  FILE *fptr;
+  int linelen;
+  int num_rapid_files;
+  bool bIsRapid;
+
+  if ((argc < 2) || (argc > 4)) {
+    printf(usage);
+    return 1;
+  }
+
+  bVerbose = false;
+  bRename = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else if (!strcmp(argv[curr_arg],"-rename"))
+      bRename = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  if (bRename)
+    num_rapid_files = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    if ((fptr = fopen(filename,"r")) == NULL) {
+      printf(couldnt_open,filename);
+      continue;
+    }
+
+    bIsRapid = false;
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&linelen,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      if (!strncmp(line,"[TimeControl ",13)) {
+        for (n = 0; n < NUM_RAPID_TIME_CONTROLS; n++) {
+          if (!strncmp(&line[14],rapid_time_controls[n],5))
+            break;
+        }
+
+        if (n < NUM_RAPID_TIME_CONTROLS) {
+          if (!bRename)
+            printf("%s\n",filename);
+          else {
+            num_rapid_files++;
+            bIsRapid = true;
+          }
+        }
+
+        break;
+      }
+    }
+
+    fclose(fptr);
+
+    if (bRename) {
+      if (bIsRapid) {
+        sprintf(filename2,"rapid_game%05d.txt",num_rapid_files);
+        remove(filename2);
+        rename(filename,filename2);
+        printf("%s\n",filename2);
+      }
+      else {
+        printf("%s\n",filename);
+      }
+    }
+  }
+
+  fclose(fptr0);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
