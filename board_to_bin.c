53f532d351b8b3249f1f3bb57d7580bdfcbd717d
neostreet <lloyd.aidan@gmail.com>
2025-01-07 14:42:00 -0500

    add orientation argument to populate_board_from_board_file()

--- board_to_bin.c
+++ board_to_bin.c
@@ -8,32 +8,35 @@
 #include "chess.mac"
 
 static char usage[] =
-"usage: board_to_bin infile outfile\n";
+"usage: board_to_bin infile orientation outfile\n";
 
 int main(int argc,char **argv)
 {
   int n;
+  int orientation;
   unsigned char board[CHARS_IN_BOARD];
   int retval;
 
-  if (argc != 3) {
+  if (argc != 4) {
     printf(usage);
     return 1;
   }
 
-  if (!strcmp(argv[1],argv[2])) {
+  if (!strcmp(argv[1],argv[3])) {
     printf("outfile must differ from infile\n");
     return 2;
   }
 
-  retval = populate_board_from_board_file(board,argv[1]);
+  sscanf(argv[2],"%d",&orientation);
+
+  retval = populate_board_from_board_file(board,argv[1],orientation);
 
   if (retval) {
     printf("populate_board_from_board_file failed: %d\n",retval);
     return 3;
   }
 
-  write_board_to_binfile(board,argv[2]);
+  write_board_to_binfile(board,argv[3]);
 
   return 0;
 }
