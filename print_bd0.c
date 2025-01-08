dea1902ce899a8d476863e3612d1df2b669d7ce5
neostreet <lloyd.aidan@gmail.com>
2025-01-08 13:22:51 -0500

    change order of arguments

--- print_bd0.c
+++ print_bd0.c
@@ -7,16 +7,15 @@
 #include "chess.fun"
 #include "chess.mac"
 
-static char usage[] = "usage: print_bd0 orientation filename\n";
+static char usage[] = "usage: print_bd0 filename orientation\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
 
-static unsigned char board1[CHARS_IN_BOARD];
-
 int main(int argc,char **argv)
 {
   int retval;
+  unsigned char board[CHARS_IN_BOARD];
   int orientation;
 
   if (argc != 3) {
@@ -24,16 +23,16 @@ int main(int argc,char **argv)
     return 1;
   }
 
-  sscanf(argv[1],"%d",&orientation);
+  sscanf(argv[2],"%d",&orientation);
 
-  retval = populate_board_from_bin_board_file(board1,argv[2]);
+  retval = populate_board_from_bin_board_file(board,argv[1]);
 
   if (retval) {
-    printf("populate_board_from_bin_board_file() failed on %s: %d\n",argv[2],retval);
+    printf("populate_board_from_bin_board_file() failed on %s: %d\n",argv[1],retval);
     return 2;
   }
 
-  print_bd0(board1,orientation);
+  print_bd0(board,orientation);
 
   return 0;
 }
