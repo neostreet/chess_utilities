409504220353a6ebcc185cc5c181506089c6d93e
neostreet <lloyd.aidan@gmail.com>
2025-01-08 12:23:48 -0500

    add print_bd_lite.c

---
+++ print_bd_lite.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "chess.h"
+#define MAKE_GLOBALS_HERE
+#include "chess.glb"
+#include "chess.fun"
+#include "chess.mac"
+
+static char usage[] =
+"usage: print_bd_lite (-binary_format) filename\n";
+
+char couldnt_get_status[] = "couldn't get status of %s\n";
+char couldnt_open[] = "couldn't open %s\n";
+
+void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bBinaryFormat;
+  int retval;
+  struct game curr_game;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bBinaryFormat = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-binary_format"))
+      bBinaryFormat = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if (!bBinaryFormat) {
+    retval = read_game(argv[curr_arg],&curr_game,err_msg);
+
+    if (retval) {
+      printf("read_game of %s failed: %d\n",argv[curr_arg],retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+      return 3;
+    }
+  }
+  else {
+    retval = read_binary_game(argv[curr_arg],&curr_game);
+
+    if (retval) {
+      printf("read_binary_game of %s failed: %d\n",argv[curr_arg],retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+      return 4;
+    }
+  }
+
+  print_bd0(curr_game.board,curr_game.orientation);
+
+  return 0;
+}
