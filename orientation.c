be83a7c8fc7c362c6677ae1c68308895247da46c
neostreet <lloyd.aidan@gmail.com>
2025-01-20 14:26:38 -0500

    add orientation.c

---
+++ orientation.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "chess.h"
+#define MAKE_GLOBALS_HERE
+#include "chess.glb"
+#include "chess.fun"
+#include "chess.mac"
+
+static char usage[] =
+"usage: orientation (-binary_format) filename\n";
+
+int main(int argc,char **argv)
+{
+  int n;
+  int curr_arg;
+  bool bBinaryFormat;
+  int retval;
+  struct game curr_game;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bBinaryFormat = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-binary_format"))
+      bBinaryFormat = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  bzero(&curr_game,sizeof (struct game));
+
+  if (!bBinaryFormat) {
+    retval = read_game(argv[curr_arg],&curr_game,err_msg);
+
+    if (retval) {
+      printf("read_game of %s failed: %d\n",argv[curr_arg],retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+
+      return 3;
+    }
+  }
+  else {
+    retval = read_binary_game(argv[curr_arg],&curr_game);
+
+    if (retval) {
+      printf("read_binary_game of %s failed: %d\n",argv[curr_arg],retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+
+      return 4;
+    }
+  }
+
+  printf("%d %d %s\n",curr_game.orientation,curr_game.num_moves,argv[curr_arg]);
+
+  return 0;
+}
