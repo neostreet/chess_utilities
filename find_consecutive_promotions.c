f529b3b65d5e04c0d68e01bed4553e23519eb6cf
neostreet <lloyd.aidan@gmail.com>
2025-03-20 15:35:55 -0400

    add -verbose flag

--- find_consecutive_promotions.c
+++ find_consecutive_promotions.c
@@ -11,34 +11,35 @@
 static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
-"usage: find_consecutive_promotions (-debug) filename\n";
+"usage: find_consecutive_promotions (-verbose) filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
 
-static bool has_consecutive_promotions(struct game *gamept);
+static bool has_consecutive_promotions(struct game *gamept,int *promotion_move);
 static bool is_promotion(int special_move_info);
 
 int main(int argc,char **argv)
 {
   int n;
   int curr_arg;
-  bool bDebug;
+  bool bVerbose;
   int retval;
   FILE *fptr;
   int filename_len;
   struct game curr_game;
+  int promotion_move;
 
   if ((argc < 2) || (argc > 3)) {
     printf(usage);
     return 1;
   }
 
-  bDebug = false;
+  bVerbose = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
-    if (!strcmp(argv[curr_arg],"-debug"))
-      bDebug = true;
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
     else
       break;
   }
@@ -70,20 +71,27 @@ int main(int argc,char **argv)
       continue;
     }
 
-    if (has_consecutive_promotions(&curr_game))
-      printf("%s\n",filename);
+    if (has_consecutive_promotions(&curr_game,&promotion_move)) {
+      if (!bVerbose)
+        printf("%s\n",filename);
+      else
+        printf("%s %d\n",filename,promotion_move);
+    }
   }
 
   return 0;
 }
 
-static bool has_consecutive_promotions(struct game *gamept)
+static bool has_consecutive_promotions(struct game *gamept,int *promotion_move)
 {
   int n;
 
   for (n = 0; n < gamept->num_moves - 1; n++) {
-    if (is_promotion(gamept->moves[n].special_move_info) && is_promotion(gamept->moves[n + 1].special_move_info))
+    if (is_promotion(gamept->moves[n].special_move_info) && is_promotion(gamept->moves[n + 1].special_move_info)) {
+      *promotion_move = n;
+
       return true;
+    }
   }
 
   return false;
