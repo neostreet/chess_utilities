fa5e4d0a3c38e0c562dd794fe51d1bc0d3c1a48c
neostreet <lloyd.aidan@gmail.com>
2025-03-22 20:00:08 -0400

    dehardcode the "rapid" prefix

--- split_etc_b.bat
+++ split_etc_b.bat
@@ -8,5 +8,5 @@ build_ch_file .bin.lst .bin %1_games.txt.lst
 call ftxt2ch_dont %2 %1_games.txt.lst
 fch2bin %1_games.ch.lst
 
-call tabulate_games_binary_format_it rapid_games.bin.lst
-type rapid_games.bin.lst.binary_format.tabulate_games
+call tabulate_games_binary_format_it %1_games.bin.lst
+type %1_games.bin.lst.binary_format.tabulate_games
