7a3b529fa491bff54454cd64ab986b39c1a6b296
neostreet <lloyd.aidan@gmail.com>
2025-03-25 22:50:40 -0400

    remove err_msg argument from call to read_game()

--- fch2bin.c
+++ fch2bin.c
@@ -99,7 +99,7 @@ int main(int argc,char **argv)
 
     bzero(&curr_game,sizeof (struct game));
 
-    retval = read_game(filename,&curr_game,err_msg);
+    retval = read_game(filename,&curr_game);
 
     if (!bIgnoreReadErrors) {
       if (retval) {
