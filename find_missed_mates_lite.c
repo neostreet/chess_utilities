c35aac5a98d197137ec0a8eb4ee73e5c85bd015f
neostreet <lloyd.aidan@gmail.com>
2025-03-27 19:10:40 -0400

    add -both flag

--- find_missed_mates_lite.c
+++ find_missed_mates_lite.c
@@ -11,7 +11,8 @@
 static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
-"usage: find_missed_mates_lite (-verbose) (-binary_format) (-multiple) (in_a_loss) (-mine) (-opponent) filename\n";
+"usage: find_missed_mates_lite (-verbose) (-binary_format) (-multiple) (in_a_loss) (-mine) (-opponent)\n"
+"  (-both) filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
@@ -26,14 +27,19 @@ int main(int argc,char **argv)
   bool bInALoss;
   bool bMine;
   bool bOpponent;
+  bool bBoth;
   int retval;
   FILE *fptr;
   int filename_len;
   struct game curr_game;
   int num_missed_mates;
+  int my_missed_mates;
+  int opponent_missed_mates;
   int total_missed_mates;
+  int total_my_missed_mates;
+  int total_opponent_missed_mates;
 
-  if ((argc < 2) || (argc > 8)) {
+  if ((argc < 2) || (argc > 9)) {
     printf(usage);
     return 1;
   }
@@ -44,6 +50,7 @@ int main(int argc,char **argv)
   bInALoss = false;
   bMine = false;
   bOpponent = false;
+  bBoth = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-verbose"))
@@ -58,6 +65,8 @@ int main(int argc,char **argv)
       bMine = true;
     else if (!strcmp(argv[curr_arg],"-opponent"))
       bOpponent = true;
+    else if (!strcmp(argv[curr_arg],"-both"))
+      bBoth = true;
     else
       break;
   }
@@ -67,12 +76,37 @@ int main(int argc,char **argv)
     return 2;
   }
 
-  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
-    printf(couldnt_open,argv[curr_arg]);
+  if (bMine && bOpponent) {
+    printf("can't specify both -mine and -opponent\n");
     return 3;
   }
 
-  total_missed_mates = 0;
+  if (bMine && bBoth) {
+    printf("can't specify both -mine and -both\n");
+    return 4;
+  }
+
+  if (bOpponent && bBoth) {
+    printf("can't specify both -opponent and -both\n");
+    return 5;
+  }
+
+  if (bMultiple && bBoth) {
+    printf("can't specify both -multiple and -both\n");
+    return 6;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 7;
+  }
+
+  if (!bBoth)
+    total_missed_mates = 0;
+  else {
+    total_my_missed_mates = 0;
+    total_opponent_missed_mates = 0;
+  }
 
   for ( ; ; ) {
     GetLine(fptr,filename,&filename_len,MAX_FILENAME_LEN);
@@ -108,7 +142,12 @@ int main(int argc,char **argv)
         continue;
     }
 
-    num_missed_mates = 0;
+    if (!bBoth)
+      num_missed_mates = 0;
+    else {
+      my_missed_mates = 0;
+      opponent_missed_mates = 0;
+    }
 
     for (curr_game.curr_move = 1; curr_game.curr_move < curr_game.num_moves; curr_game.curr_move++) {
       if (bMine) {
@@ -135,27 +174,47 @@ int main(int argc,char **argv)
       if ((curr_game.moves[curr_game.curr_move-1].special_move_info & SPECIAL_MOVE_MATE_IN_ONE) &&
         !(curr_game.moves[curr_game.curr_move].special_move_info & SPECIAL_MOVE_MATE)) {
 
-        num_missed_mates++;
+        if (!bBoth)
+          num_missed_mates++;
+        else {
+          if (!curr_game.orientation)
+            my_missed_mates++;
+          else
+            opponent_missed_mates++;
+        }
       }
     }
 
-    if (num_missed_mates) {
+    if ((!bBoth && num_missed_mates) || (bBoth && (my_missed_mates > 0) && (opponent_missed_mates > 0))) {
       if (bMultiple && (num_missed_mates == 1))
         continue;
 
-      total_missed_mates += num_missed_mates;
+      if (!bBoth)
+        total_missed_mates += num_missed_mates;
+      else {
+        total_my_missed_mates += my_missed_mates;
+        total_opponent_missed_mates += opponent_missed_mates;
+      }
 
       if (!bVerbose)
         printf("%s\n",filename);
-      else
-        printf("%d %s\n",num_missed_mates,filename);
+      else {
+        if (!bBoth)
+          printf("%d %s\n",num_missed_mates,filename);
+        else
+          printf("%d %d %s\n",my_missed_mates,opponent_missed_mates,filename);
+      }
     }
   }
 
   fclose(fptr);
 
-  if (bVerbose)
-    printf("\n%d\n",total_missed_mates);
+  if (bVerbose) {
+    if (!bBoth)
+      printf("\n%d\n",total_missed_mates);
+    else
+      printf("\n%d %d\n",total_my_missed_mates,total_opponent_missed_mates);
+  }
 
   return 0;
 }
