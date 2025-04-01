e216c025f7826cfa79688422d9befad80d1b14e1
neostreet <lloyd.aidan@gmail.com>
2025-03-31 21:49:36 -0400

    make variable names more specific

--- read_ecos.c
+++ read_ecos.c
@@ -19,7 +19,7 @@ static char couldnt_get_status[] = "couldn't get status of %s\n";
 
 static char eco[4];
 
-int read_ecos(char *ecos_file,char **buf_pt,int *num_ecos_pt);
+int read_ecos(char *ecos_file,char **ecos_pt,int *num_ecos_pt);
 
 int main(int argc,char **argv)
 {
@@ -28,7 +28,7 @@ int main(int argc,char **argv)
   int p;
   int curr_arg;
   bool bVerbose;
-  char *buf;
+  char *ecos;
   int num_ecos;
   int retval;
 
@@ -51,7 +51,7 @@ int main(int argc,char **argv)
     return 2;
   }
 
-  retval = read_ecos(argv[curr_arg],&buf,&num_ecos);
+  retval = read_ecos(argv[curr_arg],&ecos,&num_ecos);
 
   if (retval) {
     printf("read_ecos() failed; retval = %d\n",retval);
@@ -67,24 +67,24 @@ int main(int argc,char **argv)
 
     for (n = 0; n < num_ecos; n++) {
       for (m = 0; m < 3; m++) {
-        eco[m] = buf[p++];
+        eco[m] = ecos[p++];
       }
 
       printf("%s\n",eco);
     }
   }
 
-  free(buf);
+  free(ecos);
 
   return 0;
 }
 
-int read_ecos(char *ecos_file,char **buf_pt,int *num_ecos_pt)
+int read_ecos(char *ecos_file,char **ecos_pt,int *num_ecos_pt)
 {
   struct stat statbuf;
   int bytes_to_read;
   int bytes_read;
-  char *buf;
+  char *ecos;
   int fhndl;
 
   if (stat(ecos_file,&statbuf) == -1)
@@ -97,25 +97,25 @@ int read_ecos(char *ecos_file,char **buf_pt,int *num_ecos_pt)
 
   *num_ecos_pt = bytes_to_read / 3;
 
-  if ((buf = (char *)malloc(bytes_to_read)) == NULL)
+  if ((ecos = (char *)malloc(bytes_to_read)) == NULL)
     return 3;
 
   if ((fhndl = open(ecos_file,O_BINARY | O_RDONLY,0)) == -1) {
-    free(buf);
+    free(ecos);
     return 4;
   }
 
-  bytes_read = read(fhndl,buf,bytes_to_read);
+  bytes_read = read(fhndl,ecos,bytes_to_read);
 
   if (bytes_read != bytes_to_read) {
-    free(buf);
+    free(ecos);
     close(fhndl);
     return 5;
   }
 
   close(fhndl);
 
-  *buf_pt = buf;
+  *ecos_pt = ecos;
 
   return 0;
 }
