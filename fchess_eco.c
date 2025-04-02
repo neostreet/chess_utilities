d6d3683222629febc1f07b8aacec0eb63b351828
neostreet <lloyd.aidan@gmail.com>
2025-04-02 16:58:12 -0400

    add -eco flag

--- fchess_eco.c
+++ fchess_eco.c
@@ -8,12 +8,17 @@ static char filename[MAX_FILENAME_LEN];
 static char line[MAX_LINE_LEN];
 static char date[MAX_LINE_LEN];
 
+#define MAX_ECO_LEN 3
+static char eco[MAX_ECO_LEN+1];
+
 static char usage[] =
-"usage: fchess_eco (-date) (-name) filename\n";
+"usage: fchess_opening (-date) (-name) (-truncate) (-eco) filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static char eco_str[] = "[ECO \"";
 #define ECO_STR_LEN (sizeof (eco_str) - 1)
+static char opening_str[] = "[Opening \"";
+#define OPENING_STR_LEN (sizeof (opening_str) - 1)
 static char utcdate[] = "UTCDate";
 #define UTCDATE_LEN (sizeof (utcdate) - 1)
 
@@ -21,7 +26,8 @@ static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 static int Contains(bool bCaseSens,char *line,int line_len,
   char *string,int string_len,int *index);
 static void get_date(char *date,char *line);
-static char *get_eco(char *line,int line_len,int ix);
+static char *get_opening(char *line,int line_len,int ix,bool bTruncate);
+void get_eco(char *line,int line_len,char *eco,int max_eco_len);
 
 static int dbg_line_no;
 static int afl_dbg;
@@ -33,27 +39,35 @@ int main(int argc,char **argv)
   int curr_arg;
   bool bDate;
   bool bName;
+  bool bTruncate;
+  bool bEco;
   FILE *fptr0;
   int filename_len;
   FILE *fptr;
   int line_len;
   int line_no;
   int ix;
-  char *eco;
+  char *opening;
 
-  if ((argc < 2) || (argc > 4)) {
+  if ((argc < 2) || (argc > 6)) {
     printf(usage);
     return 1;
   }
 
   bDate = false;
   bName = false;
+  bTruncate = false;
+  bEco = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-date"))
       bDate = true;
     else if (!strcmp(argv[curr_arg],"-name"))
       bName = true;
+    else if (!strcmp(argv[curr_arg],"-truncate"))
+      bTruncate = true;
+    else if (!strcmp(argv[curr_arg],"-eco"))
+      bEco = true;
     else
       break;
   }
@@ -92,22 +106,31 @@ int main(int argc,char **argv)
       if (line_no == dbg_line_no)
         afl_dbg = 1;
 
-      if (Contains(true,
+      if (bDate && Contains(true,
         line,line_len,
         utcdate,UTCDATE_LEN,
         &ix)) {
 
-        if (bDate)
-          get_date(date,line);
+        get_date(date,line);
       }
-      else if (Contains(true,
+      else if (bEco && Contains(true,
         line,line_len,
         eco_str,ECO_STR_LEN,
         &ix)) {
 
-        eco = get_eco(line,line_len,ix + ECO_STR_LEN);
+        get_eco(line,line_len,eco,MAX_ECO_LEN);
+      }
+      else if (Contains(true,
+        line,line_len,
+        opening_str,OPENING_STR_LEN,
+        &ix)) {
 
-        printf("%s",eco);
+        opening = get_opening(line,line_len,ix + OPENING_STR_LEN,bTruncate);
+
+        if (bEco)
+          printf("%s %s",eco,opening);
+        else
+          printf("%s",opening);
 
         if (bDate)
           printf(" %s",date);
@@ -195,11 +218,14 @@ static void get_date(char *date,char *line)
   date[7] = '-';
 }
 
-static char *get_eco(char *line,int line_len,int ix)
+static char *get_opening(char *line,int line_len,int ix,bool bTruncate)
 {
   int n;
 
   for (n = ix; n < line_len; n++) {
+    if (bTruncate && (line[n] == ':'))
+      break;
+
     if (line[n] == '"')
       break;
   }
@@ -208,4 +234,18 @@ static char *get_eco(char *line,int line_len,int ix)
     line[n] = 0;
 
   return &line[ix];
+}
+
+void get_eco(char *line,int line_len,char *eco,int max_eco_len)
+{
+  int n;
+
+  for (n = 0; (6 + n < line_len) && (n < max_eco_len); n++) {
+    if (line[6 + n] == '"')
+      break;
+
+    eco[n] = line[6 + n];
+  }
+
+  eco[n] = 0;
 }
