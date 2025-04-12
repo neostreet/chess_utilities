29ff917a0011c15659d25868b14a7645ea68e9db
neostreet <lloyd.aidan@gmail.com>
2025-04-11 23:27:34 -0400

    add set_result.c

---
+++ set_result.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "chess.h"
+#define MAKE_GLOBALS_HERE
+#include "chess.glb"
+#include "chess.fun"
+#include "chess.mac"
+
+static char usage[] =
+"usage: set_result (-binary_format) result filename (filename ...)\n";
+
+int main(int argc,char **argv)
+{
+  int n;
+  int curr_arg;
+  bool bBinaryFormat;
+  int retval;
+  struct game curr_game;
+  int result;
+
+  if (argc < 3) {
+    printf(usage);
+    return 1;
+  }
+
+  bBinaryFormat = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-binary_format"))
+      bBinaryFormat = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg < 2) {
+    printf(usage);
+    return 2;
+  }
+
+  sscanf(argv[curr_arg],"%d",&result);
+  curr_arg++;
+
+  for ( ; curr_arg < argc; curr_arg++) {
+    if (!bBinaryFormat) {
+      retval = read_game(argv[curr_arg],&curr_game);
+
+      if (retval) {
+        printf("read_game of %s failed: %d\n",argv[curr_arg],retval);
+        printf("curr_move = %d\n",curr_game.curr_move);
+
+        return 3;
+      }
+    }
+    else {
+      retval = read_binary_game(argv[curr_arg],&curr_game);
+
+      if (retval) {
+        printf("read_binary_game of %s failed: %d\n",argv[curr_arg],retval);
+        printf("curr_move = %d\n",curr_game.curr_move);
+
+        return 4;
+      }
+    }
+
+    curr_game.result = result;
+
+    retval = write_binary_game(argv[curr_arg],&curr_game);
+
+    if (retval) {
+      printf("write_binary_game of %s failed: %d\n",argv[curr_arg],retval);
+      return 5;
+    }
+  }
+
+  return 0;
+}
