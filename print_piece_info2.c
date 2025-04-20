d11a8400fb1fc39af7709d4883deda837e113b99
neostreet <lloyd.aidan@gmail.com>
2025-04-20 19:00:55 -0400

    add -raw flag

--- print_piece_info2.c
+++ print_piece_info2.c
@@ -8,7 +8,7 @@
 #include "chess.mac"
 
 static char usage[] =
-"usage: print_piece_info2 (-debug) (-binary_format) (-abbrev) (-only_remaining) filename\n";
+"usage: print_piece_info2 (-debug) (-binary_format) (-abbrev) (-only_remaining) (-raw) filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
@@ -17,15 +17,17 @@ void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 
 int main(int argc,char **argv)
 {
+  int n;
   int curr_arg;
   bool bDebug;
   bool bBinaryFormat;
   bool bAbbrev;
   bool bOnlyRemaining;
+  bool bRaw;
   int retval;
   struct game curr_game;
 
-  if ((argc < 2) || (argc > 6)) {
+  if ((argc < 2) || (argc > 7)) {
     printf(usage);
     return 1;
   }
@@ -34,6 +36,7 @@ int main(int argc,char **argv)
   bBinaryFormat = false;
   bAbbrev = false;
   bOnlyRemaining = false;
+  bRaw = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-debug"))
@@ -44,6 +47,8 @@ int main(int argc,char **argv)
       bAbbrev = true;
     else if (!strcmp(argv[curr_arg],"-only_remaining"))
       bOnlyRemaining = true;
+    else if (!strcmp(argv[curr_arg],"-raw"))
+      bRaw = true;
     else
       break;
   }
@@ -80,10 +85,30 @@ int main(int argc,char **argv)
   putchar(0x0a);
 
   printf("White:\n");
-  print_piece_info2(curr_game.white_pieces,true,bAbbrev,bOnlyRemaining);
+
+  if (!bRaw)
+    print_piece_info2(curr_game.white_pieces,true,bAbbrev,bOnlyRemaining);
+  else {
+    for (n = 0; n < NUM_PIECES_PER_PLAYER; n++) {
+      printf("  %d %d %d\n",
+        curr_game.white_pieces[n].piece_id,
+        curr_game.white_pieces[n].current_board_position,
+        curr_game.white_pieces[n].move_count);
+    }
+  }
 
   printf("Black:\n");
-  print_piece_info2(curr_game.black_pieces,false,bAbbrev,bOnlyRemaining);
+
+  if (!bRaw)
+    print_piece_info2(curr_game.black_pieces,false,bAbbrev,bOnlyRemaining);
+  else {
+    for (n = 0; n < NUM_PIECES_PER_PLAYER; n++) {
+      printf("  %d %d %d\n",
+        curr_game.black_pieces[n].piece_id,
+        curr_game.black_pieces[n].current_board_position,
+        curr_game.black_pieces[n].move_count);
+    }
+  }
 
   return 0;
 }
