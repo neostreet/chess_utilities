fb7b79e869c578b501af3dc7b6dee306fc9d11c2
neostreet <lloyd.aidan@gmail.com>
2025-04-20 19:41:18 -0400

    automate the creation of the altered board

--- fbin2cmp.c
+++ fbin2cmp.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define MAX_FILENAME_LEN 1024
@@ -9,6 +10,8 @@ static char cmp_filename[MAX_FILENAME_LEN];
 #define MAX_LINE_LEN 8192
 static char line[MAX_LINE_LEN];
 
+#define MAX_CANDIDATES 24
+
 #include "chess.h"
 #define MAKE_GLOBALS_HERE
 #include "chess.glb"
@@ -16,10 +19,11 @@ static char line[MAX_LINE_LEN];
 #include "chess.mac"
 
 static char usage[] =
-"usage: fbin2cmp (-debug) filename\n";
+"usage: fbin2cmp (-debug) seed filename\n";
 
 static struct game curr_game;
 static struct board_comparison curr_comparison;
+static unsigned char altered_board[CHARS_IN_BOARD];
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
@@ -32,14 +36,21 @@ static int build_cmp_filename(
 
 int main(int argc,char **argv)
 {
+  int n;
   int curr_arg;
   bool bDebug;
+  int seed;
+  int work;
+  int modulo1;
+  int modulo2;
   FILE *fptr0;
   int file_len;
   int bin_filename_len;
   int retval;
+  int candidates[MAX_CANDIDATES];
+  int num_candidates;
 
-  if ((argc < 2) || (argc > 3)) {
+  if ((argc < 3) || (argc > 4)) {
     printf(usage);
     return 1;
   }
@@ -53,13 +64,16 @@ int main(int argc,char **argv)
       break;
   }
 
-  if (argc - curr_arg != 1) {
+  if (argc - curr_arg != 2) {
     printf(usage);
     return 2;
   }
 
-  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
-    printf(couldnt_open,argv[curr_arg]);
+  sscanf(argv[1],"%d",&seed);
+  srand(seed);
+
+  if ((fptr0 = fopen(argv[curr_arg+1],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg+1]);
     return 3;
   }
 
@@ -68,6 +82,9 @@ int main(int argc,char **argv)
 
     if (feof(fptr0))
       break;
+
+    work = rand();
+    modulo1 = work % 2;
 
     bin_filename_len = strlen(filename);
 
@@ -88,9 +105,43 @@ int main(int argc,char **argv)
       continue;
     }
 
+    num_candidates = 0;
+
+    for (n = 0; n < NUM_PIECES_PER_PLAYER; n++) {
+      if (curr_game.white_pieces[n].current_board_position != -1) {
+        if ((curr_game.white_pieces[n].piece_id == PAWN_ID) ||
+          (curr_game.white_pieces[n].piece_id == KNIGHT_ID) ||
+          (curr_game.white_pieces[n].piece_id == BISHOP_ID)) {
+
+          candidates[num_candidates++] = curr_game.white_pieces[n].current_board_position;
+        }
+      }
+
+      if (curr_game.black_pieces[n].current_board_position != -1) {
+        if ((curr_game.black_pieces[n].piece_id == PAWN_ID * -1) ||
+          (curr_game.black_pieces[n].piece_id == KNIGHT_ID * -1) ||
+          (curr_game.black_pieces[n].piece_id == BISHOP_ID * -1)) {
+
+          candidates[num_candidates++] = curr_game.black_pieces[n].current_board_position;
+        }
+      }
+    }
+
+    modulo2 = work % num_candidates;
+
+    copy_board(curr_game.board,altered_board);
+    set_piece1(altered_board,candidates[modulo2],0);
+
     curr_comparison.orientation = curr_game.orientation;
-    copy_board(curr_game.board,curr_comparison.board[0]);
-    copy_board(curr_game.board,curr_comparison.board[1]); // for now
+
+    if (!modulo1) {
+      copy_board(curr_game.board,curr_comparison.board[0]);
+      copy_board(altered_board,curr_comparison.board[1]);
+    }
+    else {
+      copy_board(curr_game.board,curr_comparison.board[1]);
+      copy_board(altered_board,curr_comparison.board[0]);
+    }
 
     retval = write_board_comparison(cmp_filename,&curr_comparison);
 
