b2f1f7ebafedf889b314593832264e1a1567b482
neostreet <lloyd.aidan@gmail.com>
2025-06-12 18:18:07 -0400

    add -max_diff and -go_to_move flags

--- fprint_force_counts.c
+++ fprint_force_counts.c
@@ -11,7 +11,8 @@
 static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
-"usage: fprint_force_counts (-binary_format) (-i_am_white) (-i_am_black) (-my_count_first) (-diff_first) filename\n";
+"usage: fprint_force_counts (-binary_format) (-i_am_white) (-i_am_black) (-my_count_first) (-diff_first)\n"
+"  (-max_diff) (-go_to_movemove) filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
@@ -25,14 +26,22 @@ int main(int argc,char **argv)
   bool bIAmBlack;
   bool bMyCountFirst;
   bool bDiffFirst;
+  bool bMaxDiff;
+  bool bGoToMove;
+  int go_to_move;
   int retval;
   FILE *fptr;
   int filename_len;
   struct game curr_game;
-  int first_ix;
-  int second_ix;
+  int first_count;
+  int second_count;
+  int max_first_count;
+  int max_second_count;
+  int max_diff;
+  int curr_diff;
+  int move_number;
 
-  if ((argc < 2) || (argc > 7)) {
+  if ((argc < 2) || (argc > 9)) {
     printf(usage);
     return 1;
   }
@@ -42,6 +51,8 @@ int main(int argc,char **argv)
   bIAmBlack = false;
   bMyCountFirst = false;
   bDiffFirst = false;
+  bMaxDiff = false;
+  bGoToMove = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-binary_format"))
@@ -54,6 +65,12 @@ int main(int argc,char **argv)
       bMyCountFirst = true;
     else if (!strcmp(argv[curr_arg],"-diff_first"))
       bDiffFirst = true;
+    else if (!strcmp(argv[curr_arg],"-max_diff"))
+      bMaxDiff = true;
+    else if (!strncmp(argv[curr_arg],"-go_to_move",11)) {
+      bGoToMove = true;
+      sscanf(&argv[curr_arg][11],"%d",&go_to_move);
+    }
     else
       break;
   }
@@ -68,9 +85,14 @@ int main(int argc,char **argv)
     return 3;
   }
 
+  if (bMaxDiff && bGoToMove) {
+    printf("can't specify both -max_diff and -go_to_move\n");
+    return 4;
+  }
+
   if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
     printf(couldnt_open,argv[curr_arg]);
-    return 4;
+    return 5;
   }
 
   for ( ; ; ) {
@@ -108,36 +130,130 @@ int main(int argc,char **argv)
     if (bIAmBlack && !curr_game.orientation)
       continue;
 
-    if (!bMyCountFirst) {
-      first_ix = WHITE;
-      second_ix = BLACK;
+    if (bGoToMove) {
+      if (go_to_move < 0)
+        go_to_move = 0;
+      else if (go_to_move > curr_game.num_moves)
+        go_to_move = curr_game.num_moves;
+
+      position_game(&curr_game,go_to_move);
     }
-    else {
-      if (!curr_game.orientation) {
-        first_ix = WHITE;
-        second_ix = BLACK;
+
+    if (!bMaxDiff) {
+      if (!bMyCountFirst) {
+        first_count = force_count[WHITE];
+        second_count = force_count[BLACK];
       }
       else {
-        first_ix = BLACK;
-        second_ix = WHITE;
+        if (!curr_game.orientation) {
+          first_count = force_count[WHITE];
+          second_count = force_count[BLACK];
+        }
+        else {
+          first_count = force_count[BLACK];
+          second_count = force_count[WHITE];
+        }
+      }
+    }
+    else {
+      max_diff = -1;
+
+      for (curr_game.curr_move = 0; curr_game.curr_move < curr_game.num_moves; curr_game.curr_move++) {
+        update_board(&curr_game,NULL,NULL,false);
+        calculate_force_counts(&curr_game);
+
+        if (!bMyCountFirst) {
+          first_count = force_count[WHITE];
+          second_count = force_count[BLACK];
+        }
+        else {
+          if (!curr_game.orientation) {
+            first_count = force_count[WHITE];
+            second_count = force_count[BLACK];
+          }
+          else {
+            first_count = force_count[BLACK];
+            second_count = force_count[WHITE];
+          }
+        }
+
+        curr_diff = first_count - second_count;
+
+        if (curr_diff > max_diff) {
+          max_diff = curr_diff;
+          max_first_count = first_count;
+          max_second_count = second_count;
+          move_number = curr_game.curr_move;
+        }
+      }
+
+      if (max_diff == -1) {
+        max_diff = 0;
+        max_first_count = 39;
+        max_second_count = 39;
+        move_number = 0;
       }
     }
 
-    if (!bDiffFirst) {
-      printf("%d %d (%d) %s %s\n",
-        force_count[first_ix],
-        force_count[second_ix],
-        force_count[first_ix] - force_count[second_ix],
-        filename,
-        curr_game.date);
+    if (!bMaxDiff) {
+      if (!bDiffFirst) {
+        if (!bGoToMove) {
+          printf("%d %d (%d) %s %s\n",
+            first_count,
+            second_count,
+            first_count - second_count,
+            filename,
+            curr_game.date);
+        }
+        else {
+          printf("%d %d (%d) %s move %d %s\n",
+            first_count,
+            second_count,
+            first_count - second_count,
+            filename,
+            go_to_move,
+            curr_game.date);
+        }
+      }
+      else {
+        if (!bGoToMove) {
+          printf("%d %d %d %s %s\n",
+            first_count - second_count,
+            first_count,
+            second_count,
+            filename,
+            curr_game.date);
+        }
+        else {
+          printf("%d %d %d %s move %d %s\n",
+            first_count - second_count,
+            first_count,
+            second_count,
+            filename,
+            go_to_move,
+            curr_game.date);
+        }
+      }
     }
     else {
-      printf("%d %d %d %s %s\n",
-        force_count[first_ix] - force_count[second_ix],
-        force_count[first_ix],
-        force_count[second_ix],
-        filename,
-        curr_game.date);
+      if (!bDiffFirst) {
+        printf("%d %d (%d) %s move %d %s\n",
+          max_first_count,
+          max_second_count,
+          max_diff,
+          filename,
+          move_number,
+          curr_game.date);
+      }
+      else {
+        printf("%d %d %d %s move %d %s\n",
+          max_diff,
+          max_first_count,
+          max_second_count,
+          filename,
+          move_number,
+          curr_game.date);
+      }
     }
   }
 
