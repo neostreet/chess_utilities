4ec753d246f8d620e8df3f5d15a231fdc3826185
neostreet <lloyd.aidan@gmail.com>
2025-06-28 17:33:09 -0400

    remove -binary_format flag

--- find_skeleton.c
+++ find_skeleton.c
@@ -11,16 +11,14 @@
 static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
-"usage: find_skeleton (-binary_format) (-i_am_white) (-i_am_black) filename\n";
+"usage: find_skeleton (-i_am_white) (-i_am_black) filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
 
 int main(int argc,char **argv)
 {
-  int n;
   int curr_arg;
-  bool bBinaryFormat;
   bool bIAmWhite;
   bool bIAmBlack;
   int retval;
@@ -28,19 +26,16 @@ int main(int argc,char **argv)
   int filename_len;
   struct game curr_game;
 
-  if ((argc < 2) || (argc > 5)) {
+  if ((argc < 2) || (argc > 4)) {
     printf(usage);
     return 1;
   }
 
-  bBinaryFormat = false;
   bIAmWhite = false;
   bIAmBlack = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
-    if (!strcmp(argv[curr_arg],"-binary_format"))
-      bBinaryFormat = true;
-    else if (!strcmp(argv[curr_arg],"-i_am_white"))
+    if (!strcmp(argv[curr_arg],"-i_am_white"))
       bIAmWhite = true;
     else if (!strcmp(argv[curr_arg],"-i_am_black"))
       bIAmBlack = true;
@@ -71,25 +66,13 @@ int main(int argc,char **argv)
 
     bzero(&curr_game,sizeof (struct game));
 
-    if (!bBinaryFormat) {
-      retval = read_game(filename,&curr_game);
+    retval = read_game(filename,&curr_game);
 
-      if (retval) {
-        printf("read_game of %s failed: %d\n",filename,retval);
-        printf("curr_move = %d\n",curr_game.curr_move);
+    if (retval) {
+      printf("read_game of %s failed: %d\n",filename,retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
 
-        continue;
-      }
-    }
-    else {
-      retval = read_binary_game(filename,&curr_game);
-
-      if (retval) {
-        printf("read_binary_game of %s failed: %d\n",filename,retval);
-        printf("curr_move = %d\n",curr_game.curr_move);
-
-        continue;
-      }
+      continue;
     }
 
     if (bIAmWhite && curr_game.orientation)
@@ -98,17 +81,7 @@ int main(int argc,char **argv)
     if (bIAmBlack && !curr_game.orientation)
       continue;
 
-#ifdef UNDEF
-    set_initial_board(&curr_game);
-
-    for (curr_game.curr_move = 0; curr_game.curr_move < curr_game.num_moves; curr_game.curr_move++) {
-      update_board(&curr_game,NULL,NULL,false);
-      update_piece_info(&curr_game);
-    }
-#endif
-
     printf("%s\n",filename);
-    print_piece_info(&curr_game);
   }
 
   fclose(fptr);
