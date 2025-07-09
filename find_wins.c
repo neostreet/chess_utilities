d7d21f0b1f318325f02cc039d4fdcd2916f182f5
neostreet <lloyd.aidan@gmail.com>
2025-07-09 17:12:48 -0400

    remove -binary_format flag and add -century and -verbose flags

--- find_wins.c
+++ find_wins.c
@@ -11,7 +11,7 @@
 static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
-"usage: find_wins (-binary_format) (-i_am_white) (-i_am_black) filename\n";
+"usage: find_wins (-i_am_white) (-i_am_black) (-century) (-verbose) filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
@@ -19,30 +19,34 @@ char couldnt_open[] = "couldn't open %s\n";
 int main(int argc,char **argv)
 {
   int curr_arg;
-  bool bBinaryFormat;
   bool bIAmWhite;
   bool bIAmBlack;
+  bool bCentury;
+  bool bVerbose;
   int retval;
   FILE *fptr;
   int filename_len;
   struct game curr_game;
 
-  if ((argc < 2) || (argc > 5)) {
+  if ((argc < 2) || (argc > 6)) {
     printf(usage);
     return 1;
   }
 
-  bBinaryFormat = false;
   bIAmWhite = false;
   bIAmBlack = false;
+  bCentury = false;
+  bVerbose = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
-    if (!strcmp(argv[curr_arg],"-binary_format"))
-      bBinaryFormat = true;
-    else if (!strcmp(argv[curr_arg],"-i_am_white"))
+    if (!strcmp(argv[curr_arg],"-i_am_white"))
       bIAmWhite = true;
     else if (!strcmp(argv[curr_arg],"-i_am_black"))
       bIAmBlack = true;
+    else if (!strcmp(argv[curr_arg],"-century"))
+      bCentury = true;
+    else if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
     else
       break;
   }
@@ -70,26 +74,17 @@ int main(int argc,char **argv)
 
     bzero(&curr_game,sizeof (struct game));
 
-    if (!bBinaryFormat) {
-      retval = read_game(filename,&curr_game);
+    retval = read_binary_game(filename,&curr_game);
 
-      if (retval) {
-        printf("read_game of %s failed: %d\n",filename,retval);
-        printf("curr_move = %d\n",curr_game.curr_move);
+    if (retval) {
+      printf("read_binary_game of %s failed: %d\n",filename,retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
 
-        continue;
-      }
+      continue;
     }
-    else {
-      retval = read_binary_game(filename,&curr_game);
 
-      if (retval) {
-        printf("read_binary_game of %s failed: %d\n",filename,retval);
-        printf("curr_move = %d\n",curr_game.curr_move);
-
-        continue;
-      }
-    }
+    if (curr_game.result != RESULT_WIN)
+      continue;
 
     if (bIAmWhite && curr_game.orientation)
       continue;
@@ -97,8 +92,19 @@ int main(int argc,char **argv)
     if (bIAmBlack && !curr_game.orientation)
       continue;
 
-    if (curr_game.result == RESULT_WIN)
+    if (bCentury) {
+      if (curr_game.opponent_elo_before - curr_game.my_elo_before < 100)
+        continue;
+    }
+
+    if (!bVerbose || !bCentury)
       printf("%s\n",filename);
+    else {
+      printf("%d (%d %d) %s\n",
+        curr_game.opponent_elo_before - curr_game.my_elo_before,
+        curr_game.opponent_elo_before,curr_game.my_elo_before,
+        filename);
+    }
   }
 
   fclose(fptr);
