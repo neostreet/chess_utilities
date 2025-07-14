23218f1913503a4fd10746e8e199355608f6233c
neostreet <lloyd.aidan@gmail.com>
2025-07-14 15:24:47 -0400

    remove -by_player flag and add -mine and -not_mine flags

--- promotions.c
+++ promotions.c
@@ -11,7 +11,7 @@
 static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
-"usage: promotions (-by_player) (-none) (-maskmask) filename\n";
+"usage: promotions (-mine) (-not_mine) (-none) (-maskmask) filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
@@ -20,31 +20,32 @@ int main(int argc,char **argv)
 {
   int n;
   int curr_arg;
-  bool bByPlayer;
+  bool bMine;
+  bool bNotMine;
   bool bNone;
   int retval;
   FILE *fptr;
   int filename_len;
   struct game curr_game;
-  bool bBlack;
-  int num_promotions1;
-  int num_promotions2;
-  int total_num_promotions1;
-  int total_num_promotions2;
+  int num_promotions;
+  int total_num_promotions;
   int mask;
 
-  if ((argc < 2) || (argc > 5)) {
+  if ((argc < 2) || (argc > 6)) {
     printf(usage);
     return 1;
   }
 
-  bByPlayer = false;
+  bMine = false;
+  bNotMine = false;
   bNone = false;
   mask = 0;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
-    if (!strcmp(argv[curr_arg],"-by_player"))
-      bByPlayer = true;
+    if (!strcmp(argv[curr_arg],"-mine"))
+      bMine = true;
+    else if (!strcmp(argv[curr_arg],"-not_mine"))
+      bNotMine = true;
     else if (!strcmp(argv[curr_arg],"-none"))
       bNone = true;
     else if (!strncmp(argv[curr_arg],"-mask",5))
@@ -58,9 +59,14 @@ int main(int argc,char **argv)
     return 2;
   }
 
+  if ((bMine && bNotMine) || (bMine && bNone) || (bNotMine && bNone)) {
+    printf("can only specify one of -mine, -not_mine, and -none\n");
+    return 3;
+  }
+
   if ((fptr = fopen(argv[argc-1],"r")) == NULL) {
     printf(couldnt_open,argv[argc-1]);
-    return 3;
+    return 4;
   }
 
   if (!mask) {
@@ -68,8 +74,7 @@ int main(int argc,char **argv)
       SPECIAL_MOVE_PROMOTION_KNIGHT | SPECIAL_MOVE_PROMOTION_BISHOP;
   }
 
-  total_num_promotions1 = 0;
-  total_num_promotions2 = 0;
+  total_num_promotions = 0;
 
   for ( ; ; ) {
     GetLine(fptr,filename,&filename_len,MAX_FILENAME_LEN);
@@ -85,57 +90,56 @@ int main(int argc,char **argv)
       continue;
     }
 
-    num_promotions1 = 0;
-    num_promotions2 = 0;
+    num_promotions = 0;
 
     for (curr_game.curr_move = 0; curr_game.curr_move < curr_game.num_moves; curr_game.curr_move++) {
-      bBlack = curr_game.curr_move & 0x1;
-
       if (curr_game.moves[curr_game.curr_move].special_move_info & mask) {
-        if (!bByPlayer) {
-          if (bBlack)
-            num_promotions2++;
-          else
-            num_promotions1++;
-        }
-        else {
-          if (!curr_game.orientation) {
-            if (bBlack)
-              num_promotions2++;
+        if (bMine) {
+          if (curr_game.num_moves % 2) {
+            if (!curr_game.orientation)
+              ;
             else
-              num_promotions1++;
+              continue;
           }
           else {
-            if (bBlack)
-              num_promotions1++;
+            if (curr_game.orientation)
+              ;
             else
-              num_promotions2++;
+              continue;
           }
         }
+        else if (bNotMine) {
+          if (curr_game.num_moves % 2) {
+            if (curr_game.orientation)
+              ;
+            else
+              continue;
+          }
+          else {
+            if (!curr_game.orientation)
+              ;
+            else
+              continue;
+          }
+        }
+
+        num_promotions++;
       }
     }
 
-    if ((!bNone && (num_promotions1 + num_promotions2)) || (bNone && !(num_promotions1 + num_promotions2))) {
-      if (!bByPlayer) {
-        printf("%3d white, %3d black, %3d total %s\n",
-          num_promotions1,num_promotions2,num_promotions1 + num_promotions2,filename);
-      }
-      else {
-        printf("%3d me, %3d opponent, %3d total %s\n",
-          num_promotions1,num_promotions2,num_promotions1 + num_promotions2,filename);
-      }
-
-      total_num_promotions1 += num_promotions1;
-      total_num_promotions2 += num_promotions2;
+    if (!bNone && num_promotions) {
+      printf("%d %s\n",num_promotions,filename);
+      total_num_promotions += num_promotions;
     }
+
+    if (bNone && !num_promotions)
+      printf("%s\n",filename);
   }
 
   fclose(fptr);
 
-  if (!bByPlayer)
-    printf("\n%d white, %d black, %d total\n",total_num_promotions1,total_num_promotions2,total_num_promotions1 + total_num_promotions2);
-  else
-    printf("\n%d me, %d opponent, %d total\n",total_num_promotions1,total_num_promotions2,total_num_promotions1 + total_num_promotions2);
+  if (!bNone && total_num_promotions)
+    printf("\n%d total\n",total_num_promotions);
 
   return 0;
 }
