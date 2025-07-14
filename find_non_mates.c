f6688f3c5a69845a1a82d1ae984d310bfde9a6ca
neostreet <lloyd.aidan@gmail.com>
2025-07-14 19:59:08 -0400

    remove -binary_format flag

--- find_non_mates.c
+++ find_non_mates.c
@@ -11,37 +11,31 @@
 static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
-"usage: find_non_mates (-debug) (-binary_format) filename\n";
+"usage: find_non_mates (-debug) filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
-
-int get_mate_distance(int mating_square,int mated_square);
 
 int main(int argc,char **argv)
 {
   int n;
   int curr_arg;
   bool bDebug;
-  bool bBinaryFormat;
   int retval;
   FILE *fptr;
   int filename_len;
   struct game curr_game;
 
-  if ((argc < 2) || (argc > 4)) {
+  if ((argc < 2) || (argc > 3)) {
     printf(usage);
     return 1;
   }
 
   bDebug = false;
-  bBinaryFormat = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-debug"))
       bDebug = true;
-    else if (!strcmp(argv[curr_arg],"-binary_format"))
-      bBinaryFormat = true;
     else
       break;
   }
@@ -64,25 +58,13 @@ int main(int argc,char **argv)
 
     bzero(&curr_game,sizeof (struct game));
 
-    if (!bBinaryFormat) {
-      retval = read_game(filename,&curr_game);
+    retval = read_game(filename,&curr_game);
 
-      if (retval) {
-        printf("read_game of %s failed: %d\n",filename,retval);
-        printf("curr_move = %d\n",curr_game.curr_move);
+    if (retval) {
+      printf("read_game of %s failed: %d\n",filename,retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
 
-        continue;
-      }
-    }
-    else {
-      retval = read_binary_game(filename,&curr_game);
-
-      if (retval) {
-        printf("read_binary_game of %s failed: %d\n",filename,retval);
-        printf("curr_move = %d\n",curr_game.curr_move);
-
-        continue;
-      }
+      continue;
     }
 
     if (!(curr_game.moves[curr_game.num_moves-1].special_move_info & SPECIAL_MOVE_MATE)) {
