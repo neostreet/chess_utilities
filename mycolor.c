18f389c78bdaf944703327111394a646e6456954
neostreet <lloyd.aidan@gmail.com>
2025-10-30 15:25:10 -0400

    remove -binary_format flag

--- mycolor.c
+++ mycolor.c
@@ -11,7 +11,7 @@
 static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
-"usage: mycolor -binary_format [black | white] filename\n";
+"usage: mycolor [black | white] filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
@@ -19,40 +19,24 @@ char couldnt_open[] = "couldn't open %s\n";
 int main(int argc,char **argv)
 {
   int n;
-  int curr_arg;
   bool bWhite;
-  bool bBinaryFormat;
   int retval;
   FILE *fptr;
   int filename_len;
   struct game curr_game;
 
-  if ((argc < 3) || (argc > 4)) {
+  if (argc != 3) {
     printf(usage);
     return 1;
   }
 
-  bBinaryFormat = false;
-
-  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
-    if (!strcmp(argv[curr_arg],"-binary_format"))
-      bBinaryFormat = true;
-    else
-      break;
-  }
-
-  if (argc - curr_arg != 2) {
-    printf(usage);
-    return 2;
-  }
-
-  if (!strcmp(argv[curr_arg],"white"))
+  if (!strcmp(argv[1],"white"))
     bWhite = true;
-  else if (!strcmp(argv[curr_arg],"black"))
+  else if (!strcmp(argv[1],"black"))
     bWhite = false;
 
-  if ((fptr = fopen(argv[curr_arg+1],"r")) == NULL) {
-    printf(couldnt_open,argv[curr_arg+1]);
+  if ((fptr = fopen(argv[2],"r")) == NULL) {
+    printf(couldnt_open,argv[2]);
     return 2;
   }
 
@@ -62,25 +46,13 @@ int main(int argc,char **argv)
     if (feof(fptr))
       break;
 
-    if (!bBinaryFormat) {
-      retval = read_game(filename,&curr_game);
+    retval = read_game(filename,&curr_game);
 
-      if (retval) {
-        printf("read_game of %s failed: %d\n",filename,retval);
-        printf("curr_move = %d\n",curr_game.curr_move);
+    if (retval) {
+      printf("read_game of %s failed: %d\n",filename,retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
 
-        continue;
-      }
-    }
-    else {
-      retval = read_binary_game(filename,&curr_game);
-
-      if (retval) {
-        printf("read_binary_game of %s failed: %d\n",filename,retval);
-        printf("curr_move = %d\n",curr_game.curr_move);
-
-        continue;
-      }
+      continue;
     }
 
     if (bWhite) {
