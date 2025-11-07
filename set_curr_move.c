5941008e5592fd515ab1808d54d205134d5f3b4a
neostreet <lloyd.aidan@gmail.com>
2025-11-07 16:05:24 -0500

    add set_curr_move.c

---
+++ set_curr_move.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "chess.h"
+#define MAKE_GLOBALS_HERE
+#include "chess.glb"
+#include "chess.fun"
+#include "chess.mac"
+
+static char usage[] =
+"usage: set_curr_move curr_move filename (filename ...)\n";
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  int retval;
+  struct game curr_game;
+  int curr_move;
+
+  if (argc < 3) {
+    printf(usage);
+    return 1;
+  }
+
+  sscanf(argv[1],"%d",&curr_move);
+
+  for (curr_arg = 2; curr_arg < argc; curr_arg++) {
+    retval = read_game(argv[curr_arg],&curr_game);
+
+    if (retval) {
+      printf("read_game of %s failed: %d\n",argv[curr_arg],retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+
+      return 2;
+    }
+
+    curr_game.curr_move = curr_move;
+
+    retval = write_binary_game(argv[curr_arg],&curr_game);
+
+    if (retval) {
+      printf("write_binary_game of %s failed: %d\n",argv[curr_arg],retval);
+      return 3;
+    }
+  }
+
+  return 0;
+}
