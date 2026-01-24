957a01fb8ff04b7562e632d3fbb2fe5a2ea554c5
neostreet <lloyd.aidan@gmail.com>
2026-01-24 15:48:11 -0500

    add fmax_force_diff.c and fmin_force_diff.c

---
+++ fmax_force_diff.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "chess.h"
+#define MAKE_GLOBALS_HERE
+#include "chess.glb"
+#include "chess.fun"
+#include "chess.mac"
+
+#define MAX_FILENAME_LEN 256
+static char filename[MAX_FILENAME_LEN];
+
+static char usage[] =
+"usage: fmax_force_diff (-debug) (-i_am_white) (-i_am_black) filename\n";
+
+char couldnt_get_status[] = "couldn't get status of %s\n";
+char couldnt_open[] = "couldn't open %s\n";
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bDebug;
+  bool bIAmWhite;
+  bool bIAmBlack;
+  int retval;
+  FILE *fptr;
+  int filename_len;
+  struct game curr_game;
+  int max_force_diff;
+  int force_diff;
+  int dbg;
+
+  if ((argc < 2) || (argc > 5)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = false;
+  bIAmWhite = false;
+  bIAmBlack = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else if (!strcmp(argv[curr_arg],"-i_am_white"))
+      bIAmWhite = true;
+    else if (!strcmp(argv[curr_arg],"-i_am_black"))
+      bIAmBlack = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if (bIAmWhite and bIAmBlack) {
+    printf("can't specify both -i_am_white and -i_am_black\n");
+    return 3;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 4;
+  }
+
+  for ( ; ; ) {
+    GetLine(fptr,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr))
+      break;
+
+    bzero(&curr_game,sizeof (struct game));
+
+    retval = read_game(filename,&curr_game);
+
+    if (retval) {
+      printf("read_game of %s failed: %d\n",filename,retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+
+      continue;
+    }
+
+    if (bIAmWhite && curr_game.orientation)
+      continue;
+
+    if (bIAmBlack && !curr_game.orientation)
+      continue;
+
+    max_force_diff = 0;
+
+    set_initial_board(&curr_game);
+
+    for (curr_game.curr_move = 0;
+         curr_game.curr_move < curr_game.num_moves;
+         update_board(&curr_game,NULL,NULL,false),calculate_force_counts(&curr_game),curr_game.curr_move++) {
+
+      if (curr_game.curr_move == dbg_move)
+        dbg = 1;
+
+      if (!curr_game.orientation)
+        force_diff = force_count[WHITE] - force_count[BLACK];
+      else
+        force_diff = force_count[BLACK] - force_count[WHITE];
+
+      if (bDebug)
+        printf("move %d: force_diff = %d\n",curr_game.curr_move,force_diff);
+
+      if (force_diff > max_force_diff)
+        max_force_diff = force_diff;
+    }
+
+    printf("%d %s %s\n",max_force_diff,filename,curr_game.date);
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
