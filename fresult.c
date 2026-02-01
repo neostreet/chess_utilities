47e47f7ce8ebe15d746bcdf6f1f9494d08855bd5
neostreet <lloyd.aidan@gmail.com>
2026-02-01 15:49:07 -0500

    add -plus_minus flag

--- fresult.c
+++ fresult.c
@@ -11,7 +11,7 @@
 static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
-"usage: fresult (-decode) (-points) (-my_wins) (-my_draws) (-my_losses) filename\n";
+"usage: fresult (-my_wins) (-my_draws) (-my_losses) (-decode) (-points) (-plus_minus) filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
@@ -20,39 +20,44 @@ int main(int argc,char **argv)
 {
   int n;
   int curr_arg;
-  bool bDecode;
   bool bMyWins;
   bool bMyDraws;
   bool bMyLosses;
+  bool bDecode;
   bool bPoints;
+  bool bPlusMinus;
+  int plus_minus;
   int retval;
   FILE *fptr;
   int filename_len;
   struct game curr_game;
   bool bPrintedFilename;
 
-  if ((argc < 2) || (argc > 7)) {
+  if ((argc < 2) || (argc > 8)) {
     printf(usage);
     return 1;
   }
 
-  bDecode = false;
-  bPoints = false;
   bMyWins = false;
   bMyDraws = false;
   bMyLosses = false;
+  bDecode = false;
+  bPoints = false;
+  bPlusMinus = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
-    if (!strcmp(argv[curr_arg],"-decode"))
-      bDecode = true;
-    else if (!strcmp(argv[curr_arg],"-points"))
-      bPoints = true;
-    else if (!strcmp(argv[curr_arg],"-my_wins"))
+    if (!strcmp(argv[curr_arg],"-my_wins"))
       bMyWins = true;
     else if (!strcmp(argv[curr_arg],"-my_draws"))
       bMyDraws = true;
     else if (!strcmp(argv[curr_arg],"-my_losses"))
       bMyLosses = true;
+    else if (!strcmp(argv[curr_arg],"-decode"))
+      bDecode = true;
+    else if (!strcmp(argv[curr_arg],"-points"))
+      bPoints = true;
+    else if (!strcmp(argv[curr_arg],"-plus_minus"))
+      bPlusMinus = true;
     else
       break;
   }
@@ -62,18 +67,23 @@ int main(int argc,char **argv)
     return 2;
   }
 
-  if ((bMyWins && bMyDraws) ||
-      (bMyWins && bMyLosses) ||
-      (bMyDraws && bMyLosses)) {
-
+  if (bMyWins + bMyDraws + bMyLosses > 1) {
     printf("can only specify one of the -my_* options\n");
     return 3;
   }
 
-  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
-    printf(couldnt_open,argv[curr_arg]);
+  if (bDecode +  bPoints + bPlusMinus > 1) {
+    printf("can only specify one of -decode, -points, and -plus_minus\n");
     return 4;
   }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 5;
+  }
+
+  if (bPlusMinus)
+    plus_minus = 0;
 
   for ( ; ; ) {
     GetLine(fptr,filename,&filename_len,MAX_FILENAME_LEN);
@@ -139,6 +149,21 @@ int main(int argc,char **argv)
 
           break;
       }
+    }
+    else if (bPlusMinus) {
+      switch (curr_game.result) {
+        case RESULT_WIN:
+          plus_minus += 1;;
+
+          break;
+
+        case RESULT_LOSS:
+          plus_minus -= 1;;
+
+          break;
+      }
+
+      printf("%d %s %s\n",plus_minus,filename,curr_game.date);
     }
     else
       printf("%d %s %s\n",curr_game.result,filename,curr_game.date);
