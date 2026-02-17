160fda1ca49d604871885baee7ddb4c74a7f89e6
neostreet <lloyd.aidan@gmail.com>
2026-02-17 16:11:16 -0500

    add -geval and -leval arguments

--- felo_deltas.c
+++ felo_deltas.c
@@ -11,7 +11,7 @@
 static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
-"usage: felo_deltas (-terse_modemode) (-opponent) filename\n";
+"usage: felo_deltas (-terse_modemode) (-opponent) (-geval) (-leval) filename\n";
 
 char couldnt_get_status[] = "couldn't get status of %s\n";
 char couldnt_open[] = "couldn't open %s\n";
@@ -27,20 +27,34 @@ int main(int argc,char **argv)
   int filename_len;
   struct game curr_game;
   int elo_delta;
+  int geval;
+  bool bHaveGeval;
+  int leval;
+  bool bHaveLeval;
 
-  if ((argc < 2) || (argc > 4)) {
+  if ((argc < 2) || (argc > 6)) {
     printf(usage);
     return 1;
   }
 
   terse_mode = 0;
   bOpponent = false;
+  bHaveGeval = false;
+  bHaveLeval = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strncmp(argv[curr_arg],"-terse_mode",11))
       sscanf(&argv[curr_arg][11],"%d",&terse_mode);
     else if (!strcmp(argv[curr_arg],"-opponent"))
       bOpponent = true;
+    else if (!strncmp(argv[curr_arg],"-ge",3)) {
+      sscanf(&argv[curr_arg][3],"%d",&geval);
+      bHaveGeval = true;
+    }
+    else if (!strncmp(argv[curr_arg],"-le",3)) {
+      sscanf(&argv[curr_arg][3],"%d",&leval);
+      bHaveLeval = true;
+    }
     else
       break;
   }
@@ -50,9 +64,14 @@ int main(int argc,char **argv)
     return 2;
   }
 
+  if (bHaveGeval && bHaveLeval) {
+    printf("can't specify both -geval and -leval\n");
+    return 3;
+  }
+
   if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
     printf(couldnt_open,argv[curr_arg]);
-    return 3;
+    return 4;
   }
 
   for ( ; ; ) {
@@ -76,6 +95,15 @@ int main(int argc,char **argv)
       elo_delta = curr_game.my_elo_delta;
     else
       elo_delta = curr_game.opponent_elo_delta;
+
+    if (bHaveGeval) {
+      if (elo_delta < geval)
+        continue;
+    }
+    else if (bHaveLeval) {
+      if (elo_delta > leval)
+        continue;
+    }
 
     if (!terse_mode)
       printf("%d %s %s\n",elo_delta,filename,curr_game.date);
